5bf198bda16cb47b51527d5949aa3d0d760c9e19
Anton Kashcheev <antonkachsheev@gmail.com>
2015-07-18 18:47:40 +0300

    Implemented functions for templates::String

--- src/Others/Templates/String_Functions.h
+++ src/Others/Templates/String_Functions.h
@@ -8,17 +8,43 @@ namespace flame_ide
 {namespace templates
 {
 
-template<typename Tt>
-Tt* string_add<Tt>(size_t length_first, Tt* str_first
-				 ,size_t length_last, Tt* str_last);
+template<typename Tt> inline
+Tt* string_add<Tt>(const size_t &length_first, const Tt* &str_first
+				   ,const size_t &length_last, const Tt* &str_last);
 
 
-template<typename Tt>
-Tt* string_get_substr<Tt>(size_t length_str, Tt* str
-						,size_t pos, size_t length);
+template<typename Tt> inline
+Tt* string_get_substr<Tt>(const size_t &length_str, const Tt* &str
+						  ,const size_t &pos, const size_t &length);
 
 }}
 
+template<typename Tt>
+Tt*
+flame_ide::templates::
+string_add(const size_t &length_first, const Tt* &str_first
+		   ,const size_t &length_last, const Tt* &str_last)
+{
+	Tt* result_str = new Tt[length_first + length_last];
+	
+	std::copy_n(str_first, length_first, result_str);
+	std::copy_n(str_last, length_last, result_str+length_first);
+	
+	return result_str;
+}
+
+template<typename Tt>
+Tt*
+flame_ide::templates::
+string_get_substr(const size_t &length_str, const Tt* &str
+				  ,const size_t &pos, const size_t &length)
+{
+	Tt* result_str = new Tt[length];
+	
+	std::copy_n(str+pos, length, result_str);
+	
+	return result_str;
+}
 
 #endif // STRING_FUNCTIONS
 
