ea0cb93017f015ce6bad4cbef0948cfabb019635
Anton Kashcheev <antonkachsheev@gmail.com>
2015-10-22 17:41:42 +0300

    Not finished Array realization. No workablegit push !

--- Source/Templates/Array.h
+++ Source/Templates/Array.h
@@ -2,6 +2,7 @@
 #define TEMPLATES_ARRAY
 
 #include <Templates/SimpleArray.h>
+#include <Templates/Array_MemoryBlock.h>
 
 namespace flame_ide
 { namespace templates
@@ -10,51 +11,107 @@ namespace flame_ide
 template<class T>
 class Array : public SimpleArray<T>
 {
+	void _array_setCopy(const Array<T> &array);
+	void _array_setMove(Array<T> &array);
+	
 protected:
-	size_t arr_size;
+	size_t head, tail, size;
+	SharedPointer<MemoryBlock<T>> next, prev;
 	
 public:
 	Array();
-	Array(size_t length, const T* array);
+	Array(size_t arr_size);
 	Array(const Array<T> &array);
 	Array(Array<T> &&array);
-	Array(size_t arr_size);
 	
 	~Array();
 	
-	void pushBack(const T &data);
-	void pushFront(const T &data);
+	virtual void pushBack(const T &data);
+	virtual void pushFront(const T &data);
 	
 	void insert(size_t index, const T &data);
+#ifdef FUTURE
 	void insert(size_t index, size_t count, const T *data);
+#endif
+	virtual void popBack();
+	virtual void popFront();
 	
-	void popBack();
-	void popFront();
-	
-	void popBack(size_t count);
-	void popFront(size_t count);
+	virtual void popBack(size_t count);
+	virtual void popFront(size_t count);
 	
 	void erase(size_t index); // удаляет элемент, находящийся под этим интексом
+#ifdef FUTURE
 	void erase(size_t index, size_t count); // удаляет заданное количество элементов, находящийся за этим интексом, включая текущий
+#endif	
 	
 	void clear();
 	
-	void   setSize(size_t new_length);
+	void   setSize(size_t new_size);
 	size_t getSize() const noexcept;
+//	size_t getCapacity() const noexcept
 	
-	inline const T& at(size_t index) const;
-	inline T& at(size_t index);
+	const T& at(size_t index) const;
+	      T& at(size_t index);
 	
-	inline const T& operator[](size_t index) const;
-	inline T& operator[](size_t index);
+	const T& operator[](size_t index) const noexcept;
+	      T& operator[](size_t index)		noexcept;
 	
-	const Array<T>& operator =(const Array<T> &);
-	const Array<T>& operator =(Array<T> &&);
+	const Array<T>& operator =(const Array<T> &array);
+	const Array<T>& operator =(Array<T> &&array);
 };
 
 }}
 
-
 using namespace flame_ide::templates;
+
+template<class T>
+Array<T>::Array()
+	: SimpleArray<T>()
+{}
+
+template<class T>
+Array<T>::Array(size_t arr_size)
+	: SimpleArray<T>(arr_size)
+{}
+
+// копируем. Вместо блоков сразу аллоцирую "нарощенный" размер
+template<class T>
+Array<T>::Array(const Array<T> &array)
+	: SimpleArray<T>(array.getSize())
+{
+	size = getSize();
+	for(size_t i = 0; i < size; i++)
+	{
+		// std::copy
+		
+		// или по блокам копировать?
+	}
+}
+
+template<class T>
+Array<T>::Array(Array<T> &&array)
+{
+	// нужно понять, как SharedPointer присвоить оператором перемещения
+	// без костылей, велосипедов и std::move()
+	
+	// как вариант - попробовать отдебажить, что выберет компилятор
+	// при присваивании
+}
+
+template<class T>
+Array<T>::~Array()
+{}
+
+template<class T>
+void
+Array<T>::_array_setCopy(const Array<T> &array)
+{}
+
+template<class T>
+void
+Array<T>::_array_setMove(Array<T> &array)
+{
+	
+}
 
 #endif // TEMPLATES_ARRAY
---
+++ Source/Templates/Array_MemoryBlock.h
@@ -0,0 +1,48 @@
+#ifndef TEMPLATES_ARRAY_STRUCTURES
+#define TEMPLATES_ARRAY_STRUCTURES
+
+#include <Templates/Pointers.h>
+#include <Templates/SimpleArray.h>
+
+namespace flame_ide
+{namespace templates
+{
+
+template<class T>
+class MemoryBlock : public SimpleArray<T>
+{
+	UniquePointer<MemoryBlock<T>> next;
+	SharedPointer<MemoryBlock<T>> prev;
+	
+public:
+	MemoryBlock();
+	MemoryBlock(size_t real_size);
+	MemoryBlock(int real_size);
+	MemoryBlock(const MemoryBlock<T> &array);
+	MemoryBlock(MemoryBlock<T> &&array);
+	
+	~MemoryBlock();
+	
+	const T& operator [](size_t index) const noexcept;
+    const T& operator [](int index)    const noexcept;
+	      T& operator [](size_t index)       noexcept;
+	      T& operator [](int index)          noexcept;
+	
+	SharedPointer<MemoryBlock<T>> getNext();
+	SharedPointer<MemoryBlock<T>> getPrev();
+};
+
+}}
+
+using namespace flame_ide::templates;
+
+template<class T>
+MemoryBlock<T>::Block()
+	: SimpleArray<T>(true)
+{}
+template<class T>
+MemoryBlock<T>::Block(size_t real_size)
+	: SimpleArray<T>(real_size)
+{}
+
+#endif // TEMPLATES_ARRAY_STRUCTURES
