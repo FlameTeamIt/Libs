4cf5fda6d00e1d4185dbdbd1ba7c29a612211d42
Anton Kashcheev <antonkachsheev@gmail.com>
2016-01-12 19:25:58 +0300

    Added implementation of push* functions.

--- Source/Templates/Array_MemoryBlock.h
+++ Source/Templates/Array_MemoryBlock.h
@@ -26,22 +26,23 @@ namespace flame_ide
 template<class T>
 class MemoryBlock : public SimpleArray<T>
 {
-	inline void _block_setInit(size_t init_size);
-	inline void _block_setCopy(const MemoryBlock<T> &block);
-	inline void _block_setMove(MemoryBlock<T> &block);
+	inline void __block_setInit(size_t init_size);
+	inline void __block_setCopy(const MemoryBlock<T> &block);
+	inline void __block_setMove(MemoryBlock<T> &block);
 	
 protected:
 	typedef MemoryBlock<T> my_type;
 	
-	bool is_main;
 	bool is_end;
 	bool is_front_adding;
 	
-	size_t first_index, last_index;
 	size_t block_start_index; // start/end index in block list
 	
 	SharedPointer<my_type> prev_block;
 	SharedPointer<my_type> next_block;
+	
+	inline void _block_init_spNextBlock();
+	inline void _block_init_spPrevBlock();
 	
 public:
 	
@@ -72,6 +73,9 @@ public:
 	int pushBack(const T &obj);
 	int pushBack(T &&obj);
 	
+	int insert(size_t pos_index, const T &obj);
+	int insert(size_t pos_index, T &&obj);
+	
 	int popFront(size_t count = 1);
 	int popBack(size_t count = 1);
 	
@@ -101,93 +105,86 @@ using namespace flame_ide::templates;
 template<typename T>
 MemoryBlock<T>::MemoryBlock()
 	: SimpleArray<T>()
-	, is_main(true)
-	, is_end(false)
-	, is_front_adding(false)
-	, first_index(0)
-	, last_index(0)
-	, block_start_index(0)
-	, prev_block(make_shared<my_type>(true))
-//	, next_block(make_shared<my_type>(true))
+	,is_end(true)
+	,is_front_adding(false)
+	,block_start_index(0)
 {
-	prev_block->is_front_adding = true;
 }
 
 template<typename T>
 MemoryBlock<T>::MemoryBlock(size_t init_size)
 	: SimpleArray<T>(init_size)
-	, is_main(true)
-	, is_end(false)
-	, is_front_adding(false)
-	, first_index(0)
-	, last_index(0)
-	, block_start_index(0)
-	, prev_block(make_shared<my_type>(true))
-//	, next_block(make_shared<my_type>(true))
-{
-	
-}
+	,is_end(true)
+	,is_front_adding(false)
+	,block_start_index(0)
+{}
 
 template<typename T>
 template<typename TSize_Type>
 MemoryBlock<T>::MemoryBlock(TSize_Type init_size)
 	: SimpleArray<T>(init_size)
-	, is_main(true)
-	, is_end(false)
-	, is_front_adding(false)
-	, first_index(0)
-	, last_index(0)
-	, block_start_index(0)
-	, prev_block(make_shared<my_type>(true, init_size))
-//	, next_block(make_shared<my_type>(true))
-{
-	
-}
+	,is_end(true)
+	,is_front_adding(false)
+	,block_start_index(0)
+{}
 
 template<typename T>
 MemoryBlock<T>::MemoryBlock(bool is_front_adding)
 	: SimpleArray<T>()
-	, is_main(false)
-	, is_end(true)
-	, is_front_adding(is_front_adding)
-	, first_index(0)
-	, last_index(0)
-	, block_start_index(0)
+	,is_end(true)
+	,is_front_adding(is_front_adding)
+	,block_start_index(0)
 {}
 
 template<typename T>
 MemoryBlock<T>::MemoryBlock(bool is_front_adding, size_t init_size)
 	: SimpleArray<T>(init_size)
-	, is_main(false)
-	, is_end(true)
-	, is_front_adding(is_front_adding)
-	, first_index(is_front_adding ? init_size : 0)
-	, last_index(is_front_adding ? init_size : 0)
-	, block_start_index(0)
-{}
+	,is_end(true)
+	,is_front_adding(is_front_adding)
+	,block_start_index(0)
+{
+	this->first_index = is_front_adding ? init_size : 0;
+	this->last_index = is_front_adding ? init_size : 0;
+}
 
 template<typename T>
 template<typename TSize_Type>
 MemoryBlock<T>::MemoryBlock(bool is_front_adding, TSize_Type init_size)
 	: SimpleArray<T>(init_size)
-	, is_main(false)
-	, is_end(true)
-	, is_front_adding(is_front_adding)
-	, first_index(is_front_adding ? init_size : 0)
-	, last_index(is_front_adding ? init_size : 0)
-	, block_start_index(0)
-{}
+	,is_end(true)
+	,is_front_adding(is_front_adding)
+	,block_start_index(0)
+{
+	this->first_index = is_front_adding ? static_cast<size_t>(init_size) : 0;
+	this->last_index = is_front_adding ? static_cast<size_t>(init_size) : 0;
+}
 
 template<typename T>
 MemoryBlock<T>::~MemoryBlock()
+{}
+
+template<typename T>
+void
+MemoryBlock<T>::_block_init_spNextBlock()
 {
-	if(this->arr_capaity && this->arr_size)
+	if(!this->next_block.isInitialized())
 	{
-		array_call_distructors(this->arr_size,
-		                       this->inc_arr + first_index);
-		array_delete(this->inc_arr);
-	}
-	this->arr_capaity = 0;
+		this->next_block = make_shared<MemoryBlock>(this->arr_capaity);
+		this->next_block->prev_block = SharedPointer<MemoryBlock>(this);
+		this->is_end = false;
+	}	
+}
+
+template<typename T>
+void
+MemoryBlock<T>::_block_init_spPrevBlock()
+{
+	if(!this->prev_block.isInitialized())
+	{
+		this->prev_block = make_shared<MemoryBlock>(true, this->arr_capaity);
+		this->prev_block->next_block = SharedPointer<MemoryBlock>(this);
+		this->is_end = false;
+	}	
 }
 
 template<typename T>
@@ -216,12 +213,28 @@ MemoryBlock<T>::pushFront(const T &obj)
 {
 	if(this->is_front_adding)
 	{
-		return 1;
+		if(this->arr_size < this->arr_capaity)
+		{
+			--this->first_index;
+			++this->arr_size;
+			
+			array_copying(1, &obj, this->inc_arr + this->first_index);
+			
+			return 1;
+		}
 	}
 	else
 	{
-		return 1;
+		int &return_code = this->SimpleArray<T>::insert(0, obj);
+		if(return_code > 0)
+		{
+			return return_code;
+		}
 	}
+	
+	_block_init_spPrevBlock();
+	return prev_block->pushFront(obj);
+	
 }
 
 template<typename T>
@@ -230,13 +243,28 @@ MemoryBlock<T>::pushFront(T &&obj)
 {
 	if(this->is_front_adding)
 	{
-		return 1;
+		if(this->arr_size < this->arr_capaity)
+		{
+			--this->first_index;
+			++this->arr_size;
+			
+			inc_arr[this->first_index] = T(obj);
+			
+			return 1;
+		}
 	}
 	else
 	{
-		// нужно сдвинуть массив на один элемент вправо
-		return 1;
+		int &return_code = this->SimpleArray<T>::insert(0, obj);
+		if(return_code > 0)
+		{
+			return return_code;
+		}
 	}
+	
+	_block_init_spPrevBlock();
+	return prev_block->pushFront(obj);
+	
 }
 
 template<typename T>
@@ -246,12 +274,30 @@ MemoryBlock<T>::pushBack(const T &obj)
 	if(this->is_front_adding)
 	{
 		// нужно сдвинуть массив на один элемент влево
-		return 1;
+		if(this->arr_size < this->arr_capaity)
+		{
+			std::copy(this->inc_arr + this->first_index,
+					  this->inc_arr + this->arr_capaity,
+					  this->inc_arr + this->first_index - 1);
+			
+			array_copying(1, &obj, this->inc_arr + this->arr_capaity - 1);
+			
+			--this->first_index;
+			++this->arr_size;
+			
+			return 1;
+		}
 	}
 	else
 	{
-		return this->SimpleArray.pushBack(obj);
+		int &return_code = this->SimpleArray<T>::pushBack(obj);
+		if(return_code > 0)
+		{
+			return return_code;
+		}
 	}
+	_block_init_spNextBlock();
+	return next_block->pushBack(obj);
 }
 
 template<typename T>
@@ -260,11 +306,30 @@ MemoryBlock<T>::pushBack(T &&obj)
 {
 	if(this->is_front_adding)
 	{
-		return 1;
+		if(this->arr_size < this->arr_capaity)
+		{
+			std::copy(this->inc_arr + this->first_index,
+					  this->inc_arr + this->arr_capaity,
+					  this->inc_arr + this->first_index - 1);
+			
+			this->inc_arr[this->arr_capaity - 1].~T();
+			this->inc_arr[this->arr_capaity - 1] = T(obj);
+			
+			--this->first_index;
+			++this->arr_size;
+			
+			return 1;
+		}
 	}
 	else
 	{
-		this->SimpleArray.pushBack(obj);
+		int &return_code = this->SimpleArray<T>::pushBack(obj);
+		if(return_code < 0)
+		{
+			_block_init_spNextBlock();
+			return next_block->pushBack(obj);
+		}
+		return return_code;
 	}
 }
 
@@ -274,11 +339,34 @@ MemoryBlock<T>::popFront(size_t count)
 {
 	if(this->is_front_adding)
 	{
-		return 1;
+		if(this->arr_size - count > 0 && this->arr_capaity > 0)
+		{
+//			array_call_distructors(count, this->inc_arr + this->first_index);
+			
+//			if(prev_block.isInitialized())
+//			{
+//				// подтягиваем элементы из другого блока
+				
+				
+//				// если элементов меньше, то забираем всё
+//				// иначе тянем только нужно количество, удаляя их в старом месте
+//			}
+//			else
+//			{
+//				this->first_index += count;
+//				this->arr_size    -= count;
+//			}
+			
+			return 1;
+		}
+		else
+		{
+			return -1;
+		}
 	}
 	else
-	{
-		return 1;
+	{		
+		return this->SimpleArray<T>::erase(0, count);
 	}
 }
 
@@ -292,7 +380,7 @@ MemoryBlock<T>::popBack(size_t count)
 	}
 	else
 	{
-		return this->SimpleArray.popBack(count);
+		return 1;
 	}
 }
 
@@ -300,7 +388,7 @@ template<typename T>
 int
 MemoryBlock<T>::erase(size_t pos_index, size_t count)
 {
-	if(!this->is_front_adding)
+	if(this->is_front_adding)
 	{
 		return 1;
 	}
@@ -314,40 +402,17 @@ template<typename T>
 void
 MemoryBlock<T>::clear()
 {
-	if(is_main)
+	// don't know about pointers
+	
+	
+	this->SimpleArray<T>::clear();
+	if(is_front_adding)
 	{
-		if(prev_block.isInitialized() && !prev_block->isEmpty())
-		{
-			prev_block->clear();
-		}
 		
-		if(next_block.isInitialized() && !next_block->isEmpty())
-		{
-			next_block->clear();
-		}
 	}
 	else
 	{
-		if(is_front_adding)
-		{
-			if(prev_block.isInitialized() && !prev_block->isEmpty())
-			{
-				prev_block->clear();
-			}
-		}
-		else
-		{
-			if(next_block.isInitialized() && !next_block->isEmpty())
-			{
-				next_block->clear();
-			}
-		}
-	}
-	
-	if(this->arr_size != 0)
-	{
-		array_call_distructors(this->arr_size,
-		                       this->inc_arr + this->first_index);
+		
 	}
 }
 
@@ -355,18 +420,18 @@ template<typename T>
 const T&
 MemoryBlock<T>::operator [](size_t index) const noexcept
 {
-	return this->inc_arr
+	return *(this->inc_arr 
 	       + this->first_index
-	       + index;
+	       + index);
 }
 
 template<typename T>
 T&
 MemoryBlock<T>::operator [](size_t index) noexcept
 {
-	return this->inc_arr
+	return *(this->inc_arr 
 	       + this->first_index
-	       + index;
+	       + index);
 }
 
 template<typename T>
@@ -374,9 +439,9 @@ template<typename TSize_Type>
 const T&
 MemoryBlock<T>::operator [](TSize_Type index) const noexcept
 {
-	return this->inc_arr
+	return *(this->inc_arr
 	       + this->first_index
-	       + static_cast<size_t>(index);
+	       + static_cast<size_t>(index));
 }
 
 template<typename T>
@@ -384,9 +449,9 @@ template<typename TSize_Type>
 T&
 MemoryBlock<T>::operator [](TSize_Type index) noexcept
 {
-	return this->inc_arr
+	return *(this->inc_arr
 	       + this->first_index
-	       + static_cast<size_t>(index);
+	       + static_cast<size_t>(index));
 }
 
 template<typename T>
