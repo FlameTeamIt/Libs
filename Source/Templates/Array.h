de639c5d3a40e937967d465f9bcca40f7c92291e
Anton Kashcheev <antonkachsheev@gmail.com>
2016-01-13 03:21:20 +0300

    Deleted all implementations. Added iterator interfaces for Array and interface for Array.

--- Source/Templates/Array.h
+++ Source/Templates/Array.h
@@ -1,24 +1,31 @@
 #ifndef TEMPLATES_ARRAY
 #define TEMPLATES_ARRAY
 
-#include <Templates/SimpleArray.h>
+#include <Templates/Array_Iterators.h>
 #include <Templates/Array_MemoryBlock.h>
 
 namespace flame_ide
-{ namespace templates
+{namespace templates
 {
 
 template<class T>
-class Array : public SimpleArray<T>
+class Array
 {
-	void _array_setCopy(const Array<T> &array);
-	void _array_setMove(Array<T> &array);
+	size_t size, capacity;
+	MemoryBlock<T> my_block;
+	SharedPointer<MemoryBlock<T>> sp_head, sp_tail, sp_main;
 	
-protected:
-	size_t head, tail, size;
-	SharedPointer<MemoryBlock<T>> next, prev;
+	void __array_init(size_t);
+	void __array_setCopy(const Array<T> &array);
+	void __array_setMove(Array<T> &array);
 	
 public:
+//	typedef ArrayIterator<T>        iterator;
+//	typedef ArrayReverseIterator<T> reverse_iterator;
+	
+//	typedef const ArrayIterator<T>        const_iterator;
+//	typedef const ArrayReverseIterator<T> const_reverse_iterator;
+	
 	Array();
 	Array(size_t arr_size);
 	Array(const Array<T> &array);
@@ -26,29 +33,20 @@ public:
 	
 	~Array();
 	
-	virtual void pushBack(const T &data);
-	virtual void pushFront(const T &data);
+	int pushBack(const T &data);
+	int pushFront(const T &data);
 	
-	void insert(size_t index, const T &data);
-#ifdef FUTURE
-	void insert(size_t index, size_t count, const T *data);
-#endif
-	virtual void popBack();
-	virtual void popFront();
+	int insert(size_t index, const T &data);
+
+	int popBack(size_t count = 1);
+	int popFront(size_t count = 1);
 	
-	virtual void popBack(size_t count);
-	virtual void popFront(size_t count);
-	
-	void erase(size_t index); // удаляет элемент, находящийся под этим интексом
-#ifdef FUTURE
-	void erase(size_t index, size_t count); // удаляет заданное количество элементов, находящийся за этим интексом, включая текущий
-#endif	
-	
+	void erase(size_t index);
+
 	void clear();
 	
-	void   setSize(size_t new_size);
 	size_t getSize() const noexcept;
-//	size_t getCapacity() const noexcept
+	size_t getCapacity() const noexcept;
 	
 	const T& at(size_t index) const;
 	      T& at(size_t index);
@@ -66,36 +64,20 @@ using namespace flame_ide::templates;
 
 template<class T>
 Array<T>::Array()
-	: SimpleArray<T>()
 {}
 
 template<class T>
 Array<T>::Array(size_t arr_size)
-	: SimpleArray<T>(arr_size)
 {}
 
-// копируем. Вместо блоков сразу аллоцирую "нарощенный" размер
 template<class T>
 Array<T>::Array(const Array<T> &array)
-	: SimpleArray<T>(array.getSize())
 {
-	size = getSize();
-	for(size_t i = 0; i < size; i++)
-	{
-		// std::copy
-		
-		// или по блокам копировать?
-	}
 }
 
 template<class T>
 Array<T>::Array(Array<T> &&array)
 {
-	// нужно понять, как SharedPointer присвоить оператором перемещения
-	// без костылей, велосипедов и std::move()
-	
-	// как вариант - попробовать отдебажить, что выберет компилятор
-	// при присваивании
 }
 
 template<class T>
@@ -109,9 +91,12 @@ Array<T>::_array_setCopy(const Array<T> &array)
 
 template<class T>
 void
+Array<T>::_array_init(size_t size)
+{}
+
+template<class T>
+void
 Array<T>::_array_setMove(Array<T> &array)
-{
-	
-}
+{}
 
 #endif // TEMPLATES_ARRAY
