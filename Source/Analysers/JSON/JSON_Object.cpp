66f9d7e001a3c886a4437185d42f4745eabc93bb
Anton Kashcheev <antonkachsheev@gmail.com>
2016-01-14 23:07:50 +0300

    Commented some methods for compiling project.

--- Source/Analysers/JSON/JSON_Object.cpp
+++ Source/Analysers/JSON/JSON_Object.cpp
@@ -56,59 +56,59 @@ void
 JSON::
 Object::set(const Object &object)
 {
-	const size_t length = object.inc_arr.getSize();
-	this->inc_arr = object.inc_arr;
+//	const size_t length = object.inc_arr.getSize();
+//	this->inc_arr = object.inc_arr;
 	
-	for(size_t i = 0; i < length; i++)
-	{
-		inc_arr[i] = (Pair*)object.inc_arr[i]->getCopy();
-	}
+//	for(size_t i = 0; i < length; i++)
+//	{
+//		inc_arr[i] = (Pair*)object.inc_arr[i]->getCopy();
+//	}
 }
 
 void 
 JSON::
 Object::pushBack(const Data *data)
 {
-	if(data->getType() == PAIR)
-	{
-		inc_arr.pushBack((Pair*)data->getCopy());
-	}
+//	if(data->getType() == PAIR)
+//	{
+//		inc_arr.pushBack((Pair*)data->getCopy());
+//	}
 }
 
 void
 JSON::
 Object::pushFront(const Data *data)
 {
-	if(data->getType() == PAIR)
-	{
-		inc_arr.pushFront((Pair*)data->getCopy());
-	}
+//	if(data->getType() == PAIR)
+//	{
+//		inc_arr.pushFront((Pair*)data->getCopy());
+//	}
 }
 
 void
 JSON::
 Object::pushBack(const std::string &key, const Data *data)
 {
-	Pair *pair = new Pair(key, data);
-	inc_arr.pushBack(pair);
+//	Pair *pair = new Pair(key, data);
+//	inc_arr.pushBack(pair);
 }
 
 void
 JSON::
 Object::pushFront(const std::string &key, const Data *data)
 {
-	Pair *pair = new Pair(key, data);
-	inc_arr.pushFront(pair);
+//	Pair *pair = new Pair(key, data);
+//	inc_arr.pushFront(pair);
 }
 
 void
 JSON::
 Object::insert(const size_t &index, const Data *data)
 {
-	if(data->getType() != PAIR)
-	{ return; }
+//	if(data->getType() != PAIR)
+//	{ return; }
 	
-	inc_arr.insert(index, (Pair*)data->getCopy());
+//	inc_arr.insert(index, (Pair*)data->getCopy());
 }
 
 void
@@ -131,68 +131,68 @@ Object::insert(const size_t &index, const size_t &count, const Data **data)
 		
 	}
 	
-	inc_arr.insert(index, real_count, new_data);
+//	inc_arr.insert(index, real_count, new_data);
 }
 
 JSON::Data*
 JSON::
 Object::at(const size_t &index)
 {
-	return (inc_arr.getSize()) ? inc_arr[index] : nullptr;
+//	return (inc_arr.getSize()) ? inc_arr[index] : nullptr;
 }
 
 void
 JSON::
 Object::popBack()
 {
-	delete inc_arr[inc_arr.getSize()-1];
-	inc_arr.popBack();
+//	delete inc_arr[inc_arr.getSize()-1];
+//	inc_arr.popBack();
 }
 
 void
 JSON::
 Object::popFront()
 {
-	delete inc_arr[0];
-	inc_arr.popFront();
+//	delete inc_arr[0];
+//	inc_arr.popFront();
 }
 
 void
 JSON::
 Object::erase(const size_t &index)
 {
-	if(index >= inc_arr.getSize())
-	{ return; }
+//	if(index >= inc_arr.getSize())
+//	{ return; }
 	
-	delete inc_arr[index];
-	inc_arr.erase(index);
+//	delete inc_arr[index];
+//	inc_arr.erase(index);
 }
 
 void
 JSON::
 Object::erase(const size_t &index, const size_t &count)
 {
-	size_t max = index+count;
-	if(max > inc_arr.getSize()) return;
-	for(size_t i = index; i < max; i++)
-	{
-		delete inc_arr[i];
-	}
+//	size_t max = index+count;
+//	if(max > inc_arr.getSize()) return;
+//	for(size_t i = index; i < max; i++)
+//	{
+//		delete inc_arr[i];
+//	}
 	
-	inc_arr.erase(index, count);
+//	inc_arr.erase(index, count);
 }
 
 void
 JSON::
 Object::clear()
 {
-	size_t length = inc_arr.getSize();
-	for(size_t i = 0; i < length; i++)
-	{
-		delete inc_arr[i];
-	}
+//	size_t length = inc_arr.getSize();
+//	for(size_t i = 0; i < length; i++)
+//	{
+//		delete inc_arr[i];
+//	}
 	
-	inc_arr.clear();
+//	inc_arr.clear();
 }
 
 JSON::Data*
@@ -206,72 +206,72 @@ std::string
 JSON::
 Object::getAsString() const
 {
-	std::string out_json_str("{");
-	size_t &&length = inc_arr.getSize();
+//	std::string out_json_str("{");
+//	size_t &&length = inc_arr.getSize();
 	
-	// пока без форматирования
-	if(length)
-	{
-		length--;
+//	// пока без форматирования
+//	if(length)
+//	{
+//		length--;
 		
-		for(size_t i = 0; i < length; i++)
-		{
-			out_json_str += inc_arr[i]->getAsString() + ",";
-		}
-		out_json_str += inc_arr[length]->getAsString();
-	}
+//		for(size_t i = 0; i < length; i++)
+//		{
+//			out_json_str += inc_arr[i]->getAsString() + ",";
+//		}
+//		out_json_str += inc_arr[length]->getAsString();
+//	}
 	
-	out_json_str += "}";	
+//	out_json_str += "}";	
 	
-	return out_json_str;
+//	return out_json_str;
 }
 
 void
 JSON::
 Object::setAsString(const std::string &json_object)
 {
-	if(getDataType(json_object) != OBJECT)
-	{ return; }
+//	if(getDataType(json_object) != OBJECT)
+//	{ return; }
 
-	templates::List<std::string> &&obj_json_str = split(json_object);
+//	templates::List<std::string> &&obj_json_str = split(json_object);
 	
-	for(templates::List<std::string>::iterator it = obj_json_str.begin();
-		!it.isEnd();)
-	{
-		if(getDataType(it.getData()) != PAIR)
-		{
-			templates::List<std::string>::iterator old_it = it;
-			it++;
-			obj_json_str.erase(old_it);
-		}
-		else
-		{
-			it++;
-		}
-	}
+//	for(templates::List<std::string>::iterator it = obj_json_str.begin();
+//		!it.isEnd();)
+//	{
+//		if(getDataType(it.getData()) != PAIR)
+//		{
+//			templates::List<std::string>::iterator old_it = it;
+//			it++;
+//			obj_json_str.erase(old_it);
+//		}
+//		else
+//		{
+//			it++;
+//		}
+//	}
 	
-	inc_arr.setSize(obj_json_str.getSize());
-	size_t i = 0;
-	for(templates::List<std::string>::iterator it = obj_json_str.begin();
-		!it.isEnd();
-		++it)
-	{
-			Pair* pair= static_cast<Pair*>(getData(it.getData()));
-			inc_arr[i] = pair;
-			i++;
-	}
+////	inc_arr.setSize(obj_json_str.getSize());
+//	size_t i = 0;
+//	for(templates::List<std::string>::iterator it = obj_json_str.begin();
+//		!it.isEnd();
+//		++it)
+//	{
+//			Pair* pair= static_cast<Pair*>(getData(it.getData()));
+//			inc_arr[i] = pair;
+//			i++;
+//	}
 }
 
 void
 JSON::
 Object::setTemporary(bool is_temp) const
 {
-	this->inc_arr.setTemporary(is_temp);
+//	this->inc_arr.setTemporary(is_temp);
 }
 
 bool
 JSON::
 Object::isTemporary() const
 {
-	return inc_arr.isTemporary();
+//	return inc_arr.isTemporary();
 }
