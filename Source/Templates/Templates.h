ae1eb7051192ca2952246fbd55806186d9de0cb6
Anton Kashcheev <antonkachsheev@gmail.com>
2016-03-24 17:52:21 +0300

    Added file, whitch include all containers and types.

---
+++ Source/Templates/Templates.h
@@ -0,0 +1,14 @@
+#ifndef TEMPLATES_H
+#define TEMPLATES_H
+
+#include <Templates/Allocator.h>
+#include <Templates/Array.h>
+#include <Templates/ArrayBlocks.h>
+#include <Templates/BasicIterator.h>
+#include <Templates/ForAll.h>
+#include <Templates/List.h>
+#include <Templates/Pointers.h>
+#include <Templates/SimpleArray.h>
+#include <Templates/String.h>
+
+#endif // TEMPLATES_H
