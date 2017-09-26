94ba0a35af7ff85178effa357d30498d186e7144
Anton Kashcheev <antonkachsheev@gmail.com>
2017-09-26 15:41:09 +0300

    Updated Pointers.

--- src/Templates/Pointers.hpp
+++ src/Templates/Pointers.hpp
@@ -5,34 +5,34 @@ namespace flame_ide
 {namespace templates
 {
 
-template<class T>
+template<typename T, typename Traits, typename Allocator>
 class BasicPointer;
 
-template<class T>
+template<typename T, typename Traits, typename Allocator>
 class SharedPointer;
 
-template<class T>
+template<typename T, typename Traits, typename Allocator>
 class UniquePointer;
 
-template<class T, class U> inline
+template<typename T, typename U> inline
 BasicPointer<T> static_pointer_cast(const BasicPointer<U>& pointer) noexcept;
-template<class T, class U> inline
+template<typename T, typename U> inline
 SharedPointer<T> static_pointer_cast(const SharedPointer<U>& pointer) noexcept;
-template<class T, class U> inline
+template<typename T, typename U> inline
 UniquePointer<T> static_pointer_cast(UniquePointer<U>& pointer) noexcept;
 
-template<class T, class U> inline
+template<typename T, typename U> inline
 BasicPointer<T> dynamic_pointer_cast(const BasicPointer<U>& pointer) noexcept;
-template<class T, class U> inline
+template<typename T, typename U> inline
 SharedPointer<T> dynamic_pointer_cast(const SharedPointer<U>& pointer) noexcept;
-template<class T, class U> inline
+template<typename T, typename U> inline
 UniquePointer<T> dynamic_pointer_cast(UniquePointer<U>& pointer) noexcept;
 
-template<class T, class U> inline
+template<typename T, typename U> inline
 BasicPointer<T> reiterpret_pointer_cast(const BasicPointer<U>& pointer) noexcept;
-template<class T, class U> inline
+template<typename T, typename U> inline
 SharedPointer<T> reiterpret_pointer_cast(const SharedPointer<U>& pointer) noexcept;
-template<class T, class U> inline
+template<typename T, typename U> inline
 UniquePointer<T> reiterpret_pointer_cast(UniquePointer<U>& pointer) noexcept;
 
 }}
