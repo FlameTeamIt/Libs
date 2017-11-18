0e6ea5f83dc0eaa61ea1cc76fcf5554b7fb34601
Anton Kashcheev <antonkachsheev@gmail.com>
2017-11-18 21:27:57 +0300

    Templates.hpp: edited includes and defines.

--- src/Templates/Templates.hpp
+++ src/Templates/Templates.hpp
@@ -1,14 +1,18 @@
-#ifndef TEMPLATES_H
-#define TEMPLATES_H
+#ifndef TEMPLATES_HPP
+#define TEMPLATES_HPP
 
 #include <Templates/Allocator.hpp>
 #include <Templates/Array.hpp>
 #include <Templates/ArrayBlocks.hpp>
+#include <Templates/InitializerList.hpp>
 #include <Templates/Iterator.hpp>
-#include <Templates/Traits.hpp>
 #include <Templates/List.hpp>
+#include <Templates/ObjectViewer.hpp>
 #include <Templates/Pointers.hpp>
-#include <Templates/SimpleArray.hpp>
 #include <Templates/String.hpp>
+#include <Templates/Traits.hpp>
+#include <Templates/Utils.hpp>
+#include <Templates/Vector.hpp>
+#include <Templates/View.hpp>
 
-#endif // TEMPLATES_H
+#endif // TEMPLATES_HPP
