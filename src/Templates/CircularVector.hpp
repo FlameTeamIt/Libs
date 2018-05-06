58063cdc9d4ce9de07dc1e41000876ad5c2a9fef
Anton Kashcheev <antonkachsheev@gmail.com>
2018-05-07 01:16:05 +0300

    Fix CircularVector interface and add some implementations.

--- src/Templates/CircularVector.hpp
+++ src/Templates/CircularVector.hpp
@@ -17,21 +17,45 @@ class Iterator: public iterator_utils::BaseIterator<
 {
 public:
 	using Me = Iterator<T, Traits>;
-	using Parent = BaseIterator<
+	using Parent = iterator_utils::BaseIterator<
 		T *, IteratorCategory::RANDOM_ACCESS, IteratorAccess::NON_CONSTANT, Traits
 	>;
 
-	using typename Traits::Type;
+	using typename Parent::Type;
+	using typename Parent::Reference;
+	using typename Parent::Pointer;
+	using typename Parent::SizeType;
+	using typename Parent::SsizeType;
 
-	using typename Traits::Reference;
-	using typename Traits::ConstReference;
-	using typename Traits::MoveReference;
+	Iterator() = default;
+	Iterator(const Me &) = default;
+	Iterator(Me &&) = default;
+	~Iterator() = default;
+	Me &operator=(const Me &) = default;
+	Me &operator=(Me &&) = default;
 
-	using typename Traits::Pointer;
-	using typename Traits::PointerToConst;
+	Me &operator++();
+	Me operator++(int);
+	Me &operator--();
+	Me operator--(int);
 
-	using typename Traits::SizeType;
-	using typename Traits::SsizeType;
+	Reference operator*();
+	Pointer operator->();
+
+	template<typename IntType>
+	Me operator+(IntType integer);
+	Me operator+(Me iterator);
+
+	template<typename IntType>
+	Me operator-(IntType integer);
+	SsizeType operator-(Me integer);
+
+	bool operator==(const Me &iterator);
+	bool operator!=(const Me &iterator);
+	bool operator>(const Me &iterator);
+	bool operator<(const Me &iterator);
+	bool operator>=(const Me &iterator);
+	bool operator<=(const Me &iterator);
 
 private:
 	using Parent::wrappedIterator;
@@ -43,17 +67,13 @@ class ConstIterator: public iterator_utils::BaseIterator<
 {
 public:
 	using Me = Iterator<T, Traits>;
-	using Parent = BaseIterator<
+	using Parent = iterator_utils::BaseIterator<
 		T *, IteratorCategory::RANDOM_ACCESS, IteratorAccess::CONSTANT, Traits
 	>;
 
 	using typename Parent::Type;
 
-	using typename Parent::Reference;
 	using typename Parent::ConstReference;
-	using typename Parent::MoveReference;
-
-	using typename Parent::Pointer;
 	using typename Parent::PointerToConst;
 
 	using typename Parent::SizeType;
@@ -63,6 +83,7 @@ public:
 	ConstIterator(const Me &) = default;
 	ConstIterator(Me &&) = default;
 	~ConstIterator() = default;
+
 	Me &operator=(const Me &) = default;
 	Me &operator=(Me &&) = default;
 
@@ -90,7 +111,8 @@ public:
 private:
 	using Parent::wrappedIterator;
 };
-}
+
+} // circularvector_utils
 
 template<typename T
 	, typename Traits = ContainerTraits<T>
@@ -100,35 +122,285 @@ class CircularVector: public Traits
 {
 public:
 	using Me = CircularVector<T, Traits, Allocator>;
+	using Parent = Traits;
 
-	using typename Traits::Type;
+	using Iterator = circularvector_utils::Iterator<T, Traits>;
+	using ConstIterator = circularvector_utils::ConstIterator<T, Traits>;
+	using ReverseIterator = ReverseIterator<Iterator>;
+	using ConstReverseIterator = ConstReverseIterator<ConstIterator>;
 
-	using typename Traits::Reference;
-	using typename Traits::ConstReference;
-	using typename Traits::MoveReference;
+	using typename Parent::Type;
 
-	using typename Traits::Pointer;
-	using typename Traits::PointerToConst;
+	using typename Parent::Reference;
+	using typename Parent::ConstReference;
+	using typename Parent::MoveReference;
 
-	using typename Traits::SizeType;
-	using typename Traits::SsizeType;
+	using typename Parent::Pointer;
+	using typename Parent::PointerToConst;
 
-	CircularVector();
+	using typename Parent::SizeType;
+	using typename Parent::SsizeType;
+
+	CircularVector() noexcept;
 	CircularVector(const Me &);
-	CircularVector(Me &&);
+	CircularVector(Me &&) noexcept;
+
+	/**
+	 * @brief CircularVector
+	 * @param beginIt
+	 * @param endIt
+	 */
+	template<typename InputIterator>
+	CircularVector(InputIterator beginIt, InputIterator endIt);
+
 	~CircularVector();
+
 	Me &operator=(const Me &);
 	Me &operator=(Me &&);
 
+	/**
+	 * @brief operator []
+	 * @param index
+	 * @return
+	 */
+	Reference operator[](Types::ssize_t index);
+
+	/**
+	 * @brief operator []
+	 * @param index
+	 * @return
+	 */
+	ConstReference operator[](Types::ssize_t index) const;
+
+	/**
+	 * @brief size
+	 * @return
+	 */
 	SizeType size() const;
+
+	/**
+	 * @brief capacity
+	 * @return
+	 */
 	SizeType capacity() const;
 
+	/**
+	 * @brief pushBack
+	 * @param object
+	 */
+	void pushBack(ConstReference object);
+
+	/**
+	 * @brief pushBack
+	 * @param object
+	 */
+	void pushBack(MoveReference object);
+
+	/**
+	 * @brief pushBack
+	 * @param itBegin
+	 * @param itEnd
+	 */
+	template<typename InputIterator>
+	void pushBack(InputIterator itBegin, InputIterator itEnd);
+
+	/**
+	 * @brief pushFront
+	 * @param object
+	 */
+	void pushFront(ConstReference object);
+
+	/**
+	 * @brief pushFront
+	 * @param object
+	 */
+	void pushFront(MoveReference object);
+
+	/**
+	 * @brief pushFront
+	 * @param itBegin
+	 * @param itEnd
+	 */
+	template<typename InputIterator>
+	void pushFront(InputIterator itBegin, InputIterator itEnd);
+	/**
+	 * @brief emplaceBack
+	 * @param args
+	 */
+	template<typename ...Args>
+	void emplaceBack(Args &&...args);
+
+	/**
+	 * @brief emplaceFront
+	 * @param args
+	 */
+	template<typename ...Args>
+	void emplaceFront(Args &&...args);
+
+	/**
+	 * @brief popBack
+	 */
+	void popBack();
+
+	/**
+	 * @brief popFront
+	 */
+	void popFront();
+
+	/**
+	 * @brief begin
+	 * @return
+	 */
+	Iterator begin();
+
+	/**
+	 * @brief begin
+	 * @return
+	 */
+	ConstIterator begin() const;
+
+	/**
+	 * @brief cbegin
+	 * @return
+	 */
+	ConstIterator cbegin() const;
+
+	/**
+	 * @brief rbegin
+	 * @return
+	 */
+	ReverseIterator rbegin();
+
+	/**
+	 * @brief rbegin
+	 * @return
+	 */
+	ConstReverseIterator rbegin() const;
+
+	/**
+	 * @brief crbegin
+	 * @return
+	 */
+	ConstReverseIterator crbegin() const;
+
+	/**
+	 * @brief end
+	 * @return
+	 */
+	Iterator end();
+
+	/**
+	 * @brief end
+	 * @return
+	 */
+	ConstIterator end() const;
+
+	/**
+	 * @brief cend
+	 * @return
+	 */
+	ConstIterator cend() const;
+
+	/**
+	 * @brief rend
+	 * @return
+	 */
+	ReverseIterator rend();
+
+	/**
+	 * @brief rend
+	 * @return
+	 */
+	ConstReverseIterator rend() const;
+
+	/**
+	 * @brief crend
+	 * @return
+	 */
+	ConstReverseIterator crend() const;
+
 private:
-	T *array;
-	T *head;
-	T *tail;
-	SizeType vectorCapacity;
-};
+	SizeType realCapacity() const noexcept;
+
+	T *array; /**< */
+	T *head; /**< */
+	T *tail; /**< */
+	SizeType vectorCapacity; /**< */
+	Allocator allocator;
+
+}; // class CircularVector
+
+}}
+
+namespace flame_ide
+{namespace templates
+{
+
+namespace circularvector_utils
+{
+
+// Iterator
+
+template<typename T, typename Traits>
+Iterator<T, Traits> &Iterator<T, Traits>::operator++()
+{}
+
+template<typename T, typename Traits>
+Iterator<T, Traits> Iterator<T, Traits>::operator++(int)
+{}
+
+template<typename T, typename Traits>
+Iterator<T, Traits> &Iterator<T, Traits>::operator--()
+{}
+
+template<typename T, typename Traits>
+Iterator<T, Traits> Iterator<T, Traits>::operator--(int)
+{}
+
+template<typename T, typename Traits>
+typename Iterator<T, Traits>::Reference Iterator<T, Traits>::operator*()
+{}
+
+template<typename T, typename Traits>
+typename Iterator<T, Traits>::Pointer Iterator<T, Traits>::operator->()
+{}
+
+
+} // circularvector_utils
+
+template<typename T, typename Traits, typename Allocator>
+CircularVector<T, Traits, Allocator>::CircularVector() noexcept
+		: array(nullptr), head(nullptr), tail(nullptr) , vectorCapacity(0), allocator()
+{}
+
+template<typename T, typename Traits, typename Allocator>
+CircularVector<T, Traits, Allocator>::CircularVector(const Me &me)
+		: CircularVector<T, Traits, Allocator>()
+{
+	array = allocator.createArray(me.size() + 1);
+	head = tail = array;
+	vectorCapacity = me.size() + 1;
+}
+
+template<typename T, typename Traits, typename Allocator>
+CircularVector<T, Traits, Allocator>::CircularVector(Me &&me) noexcept
+		: array(move(me.array)), head(move(me.head)), tail(move(me.tail))
+		, vectorCapacity(move(me.vectorCapacity)), allocator(move(me.allocator))
+{
+	me.array = me.head = me.tail = nullptr;
+	me.vectorCapacity = 0;
+}
+
+
+
+// private
+
+template<typename T, typename Traits, typename Allocator>
+typename CircularVector<T, Traits, Allocator>::SizeType
+CircularVector<T, Traits, Allocator>::realCapacity() const noexcept
+{
+	return vectorCapacity;
+}
 
 }}
 
