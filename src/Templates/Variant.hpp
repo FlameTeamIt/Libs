3838b46af30f04848e9186bff548fa76d11e6925
Anton Kashcheev <antonkachsheev@gmail.com>
2018-05-09 03:48:42 +0300

    Add complete implementation of VariantStruct.

--- src/Templates/Variant.hpp
+++ src/Templates/Variant.hpp
@@ -2,6 +2,7 @@
 #define TEMPLATES_VARIANT_HPP
 
 #include <Templates/Traits.hpp>
+#include <Templates/Utils.hpp>
 
 namespace flame_ide
 {namespace templates
@@ -12,13 +13,56 @@ namespace variant_utils
 
 template<typename ...Args>
 struct VariantStruct
-{};
+{
+	using Me = VariantStruct<Args...>;
+
+	template<typename T>
+	T *get();
+
+	template<typename T>
+	const T *get() const;
+
+	template<typename T>
+	bool set(const T &object);
+
+	template<typename T>
+	bool set(T &&object);
+
+	bool assign(Types::ssize_t index, const Me &me);
+
+	bool assign(Types::ssize_t index, Me &&me);
+
+	bool reset(Types::ssize_t index);
+};
 
 template<typename Arg, typename ...Args>
 struct VariantStruct<Arg, Args...>
 {
-	union
+	using Me = VariantStruct<Arg, Args...>;
+
+	template<typename T>
+	T *get();
+
+	template<typename T>
+	const T *get() const;
+
+	template<typename T>
+	Types::ssize_t set(const T &object);
+
+	template<typename T>
+	Types::ssize_t set(T &&object);
+
+	bool assign(Types::ssize_t index, const Me &me);
+
+	bool assign(Types::ssize_t index, Me &&me);
+
+	bool reset(Types::ssize_t index);
+
+	union VariantUnion
 	{
+		~VariantUnion()
+		{}
+
 		Arg arg;
 		VariantStruct<Args...> pack;
 	} data;
@@ -26,7 +70,37 @@ struct VariantStruct<Arg, Args...>
 
 template<>
 struct VariantStruct<>
-{};
+{
+	using Me = VariantStruct<>;
+
+	template<typename T>
+	T *get();
+
+	template<typename T>
+	const T *get() const;
+
+	template<typename T>
+	Types::ssize_t set(const T &object);
+
+	template<typename T>
+	Types::ssize_t set(T &&object);
+
+	inline bool assign(Types::ssize_t, const Me &)
+	{
+		return false;
+	}
+
+	inline bool assign(Types::ssize_t, Me &&)
+	{
+		return false;
+	}
+
+	inline bool reset(Types::ssize_t)
+	{
+		return false;
+	}
+
+};
 
 }
 
@@ -59,7 +133,7 @@ public:
 
 	operator bool() const noexcept;
 
-	bool isSet() const;
+	bool isSet() const noexcept;
 
 	void reset() noexcept;
 
@@ -127,43 +201,208 @@ namespace flame_ide
 {namespace templates
 {
 
+namespace variant_utils
+{
+
 template<typename Arg, typename ...Args>
-Variant<Arg, Args...>::Variant() noexcept
+template<typename T>
+T *VariantStruct<Arg, Args...>::get()
+{
+	if (isSameTypes<T, Arg>())
+	{
+		return &data.arg;
+	}
+	else
+	{
+		return data.pack.template get<T>();
+	}
+}
+
+template<typename Arg, typename ...Args>
+template<typename T>
+const T *VariantStruct<Arg, Args...>::get() const
+{
+	if (isSameTypes<T, Arg>())
+	{
+		return &data.arg;
+	}
+	else
+	{
+		return data.pack.template get<T>();
+	}
+}
+
+template<typename Arg, typename ...Args>
+template<typename T>
+Types::ssize_t VariantStruct<Arg, Args...>::set(const T &object)
+{
+	Types::ssize_t result = 0;
+	if (isSameTypes<T, Arg>())
+	{
+		data.arg = object;
+	}
+	else
+	{
+		result = data.pack.template set(object);
+		(result < 0) ? result : ++result;
+	}
+	return result;
+}
+
+template<typename Arg, typename ...Args>
+template<typename T>
+Types::ssize_t VariantStruct<Arg, Args...>::set(T &&object)
+{
+	Types::ssize_t result = 0;
+	if (isSameTypes<T, Arg>())
+	{
+		data.arg = move(object);
+	}
+	else
+	{
+		result = data.pack.template set(move(object));
+		(result < 0) ? result : ++result;
+	}
+	return result;
+}
+
+
+template<typename Arg, typename ...Args>
+bool VariantStruct<Arg, Args...>::assign(Types::ssize_t index, const Me &me)
+{
+	bool result = true;
+	if (!index)
+	{
+		data.arg = me.data.arg;
+	}
+	else
+	{
+		result = data.template pack.assign(index - 1, me.pack);
+	}
+	return result;
+}
+
+template<typename Arg, typename ...Args>
+bool VariantStruct<Arg, Args...>::assign(Types::ssize_t index, Me &&me)
+{
+	bool result = true;
+	if (!index)
+	{
+		data.arg = move(me.data.arg);
+	}
+	else
+	{
+		result = data.template pack.assign(index - 1, move(me.pack));
+	}
+	return result;
+}
+
+template<typename Arg, typename ...Args>
+bool VariantStruct<Arg, Args...>::reset(Types::ssize_t index)
+{
+	bool result = true;
+	if (!index)
+	{
+		data.arg.~Arg();
+	}
+	else
+	{
+		result = data.template pack.reset(index - 1);
+	}
+	return result;
+}
+
+template<typename T>
+T *VariantStruct<>::get()
+{
+	return nullptr;
+}
+
+template<typename T>
+const T *VariantStruct<>::get() const
+{
+	return nullptr;
+}
+
+template<typename T>
+Types::ssize_t VariantStruct<>::set(const T &)
+{
+	return -1;
+}
+
+template<typename T>
+Types::ssize_t VariantStruct<>::set(T &&)
+{
+	return -1;
+}
+
+}
+
+template<typename Arg, typename ...Args>
+Variant<Arg, Args...>::Variant() noexcept : currentIndex(-1)
 {}
 
 template<typename Arg, typename ...Args>
 Variant<Arg, Args...>::Variant(const Me &me) noexcept
-{}
+{
+	// надо рекурсивно спуститься к нужно типу по индексу
+	// и присвоить
+}
 
 template<typename Arg, typename ...Args>
 Variant<Arg, Args...>::Variant(Me &&me) noexcept
-{}
+{
+	// надо рекурсивно спуститься к нужно типу по индексу
+	// и присвоить
+}
 
 template<typename Arg, typename ...Args>
 template<typename T>
 Variant<Arg, Args...>::Variant(const T &initObject) noexcept
-{}
+{
+	// надо рекурсивно спуститься к нужно типу по индексу
+	// и присвоить
+}
 
 template<typename Arg, typename ...Args>
 template<typename T>
 Variant<Arg, Args...>::Variant(T &&initObject)
-{}
+{
+	// надо рекурсивно спуститься к нужно типу по индексу
+	// и присвоить
+}
 
 template<typename Arg, typename ...Args>
 Variant<Arg, Args...> &Variant<Arg, Args...>::operator=(const Me &me)
-{}
+{
+	if (&me != this && me.currentIndex > 0)
+	{
+
+	}
+	return *this;
+}
 
 template<typename Arg, typename ...Args>
 Variant<Arg, Args...> &Variant<Arg, Args...>::operator=(Me &&me) noexcept
-{}
+{
+	if (&me != this && me.currentIndex > 0)
+	{
+
+	}
+	return *this;
+}
 
 template<typename Arg, typename ...Args>
 Variant<Arg, Args...>::operator bool() const noexcept
-{}
+{
+	return isSet();
+}
 
 template<typename Arg, typename ...Args>
-bool Variant<Arg, Args...>::isSet() const
-{}
+bool Variant<Arg, Args...>::isSet() const noexcept
+{
+	return currentIndex >= 0;
+}
 
 template<typename Arg, typename ...Args>
 void Variant<Arg, Args...>::reset() noexcept
