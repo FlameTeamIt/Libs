7579a5f4fff0d78e8b555d3af8173be8e72012fc
Anton Kashcheev <antonkachsheev@gmail.com>
2018-05-13 03:24:47 +0300

    Add base namespaces for (de-)serialization types.

---
+++ src/Templates/Serialization/Deserializer.hpp
@@ -0,0 +1,16 @@
+#ifndef TEMPLATES_SERIALIZATION_DESERIALIZER_HPP
+#define TEMPLATES_SERIALIZATION_DESERIALIZER_HPP
+
+namespace flame_ide
+{namespace templates
+{
+
+}}
+
+namespace flame_ide
+{namespace templates
+{
+
+}}
+
+#endif // TEMPLATES_SERIALIZATION_DESERIALIZER_HPP
