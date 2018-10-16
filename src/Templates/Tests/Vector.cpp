c09784b18d8cd022e3186f39b35b79e2176c095c
Anton Kashcheev <antonkachsheev@gmail.com>
2018-10-16 20:56:04 +0300

    Refactoring Vector tests.

--- src/Templates/Tests/Vector.cpp
+++ src/Templates/Tests/Vector.cpp
@@ -6,38 +6,6 @@
  */
 #include "Vector.hpp"
 
-#define COMPARE_VECTORS(vector1, vector2) \
-{ \
-	auto vector1ItBegin = vector1.begin(); \
-	auto vector1ItEnd = vector1.end(); \
-	auto vector2ItBegin = vector2.begin(); \
-	auto vector2ItEnd = vector2.end(); \
-\
-	auto vector1It = vector1ItBegin; \
-	auto vector2It = vector2ItBegin; \
-\
-	for (; vector1It != vector1ItEnd || vector2It != vector2ItEnd; \
-			++vector1It, ++vector2It) \
-	{ \
-		if (*vector1It != *vector2It) \
-		{ \
-			std::cout << "Failed"; \
-\
-			log << std::endl; \
-			std::cout << "vector1It (" #vector1 ") = "; \
-			lambdaPrint(*vector1It); \
-			std::cout << std::endl; \
-\
-			std::cout << "vector2It (" #vector2 ") = "; \
-			lambdaPrint(*vector2It); \
-			std::cout << std::endl; \
-\
-			return -1; \
-		} \
-	} \
-	std::cout << "Success" << std::endl; \
-}
-
 using namespace flame_ide::test;
 
 Vector::Vector() : AbstractTest("Vector")
@@ -62,206 +30,231 @@ Vector::~Vector()
 
 int Vector::vStart()
 {
-	constexpr size_t SIZE = 5;
-
 	TestClass testFirstObject{ -1000, -100, -10, 'F' };
 	TestClass testMiddleObject{ -2000, -200, -20, 'M' };
 	TestClass testLastObject{ -3000, -300, -30, 'L' };
 
-	auto lambdaPrint = [] (const TestClass &testObject) {
-		std::cout << "{ "
-				  << "long("<< testObject.getLong() << "), "
-				  << "int(" << testObject.getInt() << "), "
-				  << "short(" << testObject.getShort() << "), "
-				  << "char('" << testObject.getChar() << "') "
-				  << "}";
-	};
-
-	std::cout << "Test initializing size: ";
-	{
-		if (vector.size() != stdvector.size())
-		{
-			log << "Failed" << std::endl;
-			return -1;
-		}
-		else
-		{
-			std::cout << "Success" << std::endl;
-		}
-	}
-
-	std::cout << "Test initializing: ";
-	{
-		COMPARE_VECTORS(vector, stdvector)
-	}
-
-	std::cout << "Test reverse: ";
-	{
-		auto stdVectorItBegin = stdvector.rbegin();
-		auto stdVectorItEnd = stdvector.rend();
-		auto vectorItBegin = vector.rbegin();
-		auto vectorItEnd = vector.rend();
-
-		auto stdVectorIt = stdVectorItBegin;
-		auto vectorIt = vectorItBegin;
-
-		for (; stdVectorIt != stdVectorItEnd || vectorIt != vectorItEnd;
-				++stdVectorIt, ++vectorIt)
-		{
-			if (*stdVectorIt != *vectorIt)
+	CHECK_RESULT(doTestCase(
+			"initializing size"
+			, [&]()
 			{
-				std::cout << "Failed";
-
-				log << std::endl;
-				std::cout << "stdVectorIt = ";
-				lambdaPrint(*stdVectorIt);
-				std::cout << std::endl;
-
-				std::cout << "vectorIt = ";
-				lambdaPrint(*stdVectorIt);
-				std::cout << std::endl;
-
-				return -1;
+				if (vector.size() != stdvector.size())
+				{
+					return ResultType::FAILED;
+				}
+				else
+				{
+					return ResultType::SUCCESS;
+				}
 			}
+	));
+
+	CHECK_RESULT(doTestCase(
+		"initializing"
+		, [&]()
+		{
+			return compareContainers(vector, stdvector);
 		}
-		std::cout << "Success" << std::endl;
-	}
+	));
+
+	CHECK_RESULT(doTestCase(
+		"reverse"
+		, [&]()
+		{
+			auto vectorRange = templates::makeRange(
+					vector.rbegin(), vector.rend()
+			);
+			auto stdVectorRange = templates::makeRange(
+					stdvector.rbegin(), stdvector.rend()
+			);
+
+			return compareContainers(vectorRange, stdVectorRange);
+		}
+	));
 
 	{
 		TestClass tmp = *vector.rbegin();
 
-		std::cout << "Test popBack(): ";
+		CHECK_RESULT(doTestCase(
+			"popBack()"
+			, [&]()
+			{
+				vector.popBack();
+				stdvector.pop_back();
+				return compareContainers(vector, stdvector);
+			}
+		));
+
+		CHECK_RESULT(doTestCase(
+			"pushBack()"
+			, [&]()
+			{
+				vector.pushBack(tmp);
+				stdvector.push_back(tmp);
+				return compareContainers(vector, stdvector);
+			}
+		));
+	}
+
+	CHECK_RESULT(doTestCase(
+		"emplaceBack()"
+		, [&]()
 		{
+			vector.emplaceBack(6000, 600, 60, '6');
+			stdvector.emplace_back(6000, 600, 60, '6');
+
+			CHECK_RESULT(compareContainers(vector, stdvector));
+
 			vector.popBack();
 			stdvector.pop_back();
 
-			COMPARE_VECTORS(stdvector, vector)
+			return compareContainers(vector, stdvector);
 		}
+	));
 
-		std::cout << "Test pushBack(): ";
+	CHECK_RESULT(doTestCase(
+		"insert()"
+		, [&]()
 		{
-			vector.pushBack(tmp);
-			stdvector.push_back(tmp);
+			vector.insert(vector.begin(), testFirstObject);
+			vector.insert(vector.begin() + 3, testMiddleObject);
+			vector.insert(vector.end(), testLastObject);
 
-			COMPARE_VECTORS(vector, stdvector)
+			stdvector.insert(stdvector.begin(), testFirstObject);
+			stdvector.insert(stdvector.begin() + 3, testMiddleObject);
+			stdvector.insert(stdvector.end(), testLastObject);
+
+			return compareContainers(vector, stdvector);
 		}
-	}
+	));
 
-	std::cout << "Test emplaceBack(): ";
-	{
-		vector.emplaceBack(6000, 600, 60, '6');
-		stdvector.emplace_back(6000, 600, 60, '6');
+	CHECK_RESULT(doTestCase(
+		"erase()"
+		, [&]()
+		{
+			vector.erase(vector.begin() + 3);
+			vector.erase(vector.begin());
+			vector.erase(vector.end() - 1);
 
-		COMPARE_VECTORS(vector, stdvector)
+			stdvector.erase(stdvector.begin() + 3);
+			stdvector.erase(stdvector.begin());
+			stdvector.erase(stdvector.end() - 1);
 
-		vector.popBack();
-		stdvector.pop_back();
-	}
+			return compareContainers(vector, stdvector);
+		}
+	));
 
-	std::cout << "Test insert(): ";
-	{
-		vector.insert(vector.begin(), testFirstObject);
-		vector.insert(vector.begin() + 3, testMiddleObject);
-		vector.insert(vector.end(), testLastObject);
+	CHECK_RESULT(doTestCase(
+		"emplace"
+		, [&]()
+		{
+			TestClass tempFirstObject = testFirstObject;
+			TestClass tempMiddleObject = testMiddleObject;
+			TestClass tempLastObject = testLastObject;
 
-		stdvector.insert(stdvector.begin(), testFirstObject);
-		stdvector.insert(stdvector.begin() + 3, testMiddleObject);
-		stdvector.insert(stdvector.end(), testLastObject);
+			vector.emplace(vector.begin(), tempFirstObject);
+			vector.emplace(vector.begin() + 3, tempMiddleObject);
+			vector.emplace(vector.end(), tempLastObject);
 
-		COMPARE_VECTORS(vector, stdvector)
-	}
+			stdvector.emplace(stdvector.begin(), tempFirstObject);
+			stdvector.emplace(stdvector.begin() + 3, tempMiddleObject);
+			stdvector.emplace(stdvector.end(), tempLastObject);
 
-	std::cout << "Test erase(): ";
-	{
-		vector.erase(vector.begin() + 3);
-		vector.erase(vector.begin());
-		vector.erase(vector.end() - 1);
+			CHECK_RESULT(compareContainers(vector, stdvector));
 
-		stdvector.erase(stdvector.begin() + 3);
-		stdvector.erase(stdvector.begin());
-		stdvector.erase(stdvector.end() - 1);
+			vector.erase(vector.begin() + 3);
+			vector.erase(vector.begin());
+			vector.erase(vector.end() - 1);
 
-		COMPARE_VECTORS(vector, stdvector)
-	}
+			stdvector.erase(stdvector.begin() + 3);
+			stdvector.erase(stdvector.begin());
+			stdvector.erase(stdvector.end() - 1);
 
-	std::cout << "Test emplace: ";
-	{
-		TestClass tempFirstObject = testFirstObject;
-		TestClass tempMiddleObject = testMiddleObject;
-		TestClass tempLastObject = testLastObject;
-
-		vector.emplace(vector.begin(), tempFirstObject);
-		vector.emplace(vector.begin() + 3, tempMiddleObject);
-		vector.emplace(vector.end(), tempLastObject);
-
-		stdvector.emplace(stdvector.begin(), tempFirstObject);
-		stdvector.emplace(stdvector.begin() + 3, tempMiddleObject);
-		stdvector.emplace(stdvector.end(), tempLastObject);
-
-		COMPARE_VECTORS(vector, stdvector)
-
-		vector.erase(vector.begin() + 3);
-		vector.erase(vector.begin());
-		vector.erase(vector.end() - 1);
-
-		stdvector.erase(stdvector.begin() + 3);
-		stdvector.erase(stdvector.begin());
-		stdvector.erase(stdvector.end() - 1);
-	}
+			return compareContainers(vector, stdvector);
+		}
+	));
 
 	// Test insert(range)/erase(range)
 	{
 		TestClass testArray[] = {
 			testFirstObject, testMiddleObject, testLastObject
 		};
+		constexpr size_t countObjects = sizeof(testArray) / sizeof(testArray[0]);
+		constexpr size_t offset = countObjects;
 
-		std::cout << "Insert range to begin(): ";
-		{
-			vector.insert(vector.begin(), testArray, testArray + 3);
-			stdvector.insert(stdvector.begin(), testArray, testArray + 3);
+		CHECK_RESULT(doTestCase(
+			"insert range to begin()"
+			, [&]()
+			{
+				vector.insert(vector.begin(), testArray, testArray + countObjects);
+				stdvector.insert(stdvector.begin(), testArray, testArray + countObjects);
 
-			COMPARE_VECTORS(vector, stdvector)
-		}
+				return compareContainers(vector, stdvector);
+			}
+		));
 
-		std::cout << "Erase range from begin(): ";
-		{
-			vector.erase(vector.begin(), vector.begin() + 3);
-			stdvector.erase(stdvector.begin(), stdvector.begin() + 3);
+		CHECK_RESULT(doTestCase(
+			"erase range from begin()"
+			, [&]()
+			{
+				vector.erase(vector.begin(), vector.begin() + countObjects);
+				stdvector.erase(stdvector.begin(), stdvector.begin() + countObjects);
 
-			COMPARE_VECTORS(vector, stdvector)
-		}
+				return compareContainers(vector, stdvector);
+			}
+		));
 
-		std::cout << "Insert range to begin() + 3: ";
-		{
-			vector.insert(vector.begin() + 3, testArray, testArray + 3);
-			stdvector.insert(stdvector.begin() + 3, testArray, testArray + 3);
+		CHECK_RESULT(doTestCase(
+			std::string("insert range to begin() + offset(") + std::to_string(countObjects) + ")"
+			, [&]()
+			{
+				vector.insert(vector.begin() + countObjects
+						, testArray, testArray + countObjects);
+				stdvector.insert(stdvector.begin() + countObjects
+						, testArray, testArray + countObjects);
 
-			COMPARE_VECTORS(vector, stdvector)
-		}
+				return compareContainers(vector, stdvector);
+			}
+		));
 
-		std::cout << "Erase range from begin() + 3: ";
-		{
-			vector.erase(vector.begin() + 3, vector.begin() + 3 + 3);
-			stdvector.erase(stdvector.begin() + 3, stdvector.begin() + 3 + 3);
+		CHECK_RESULT(doTestCase(
+			std::string("erase range from begin() + offset(") + std::to_string(countObjects) + ")"
+			, [&]()
+			{
+				vector.erase(
+						vector.begin() + offset
+						, vector.begin() + offset + countObjects
+				);
+				stdvector.erase(
+						stdvector.begin() + offset
+						, stdvector.begin() + offset + countObjects
+				);
 
-			COMPARE_VECTORS(vector, stdvector)
-		}
+				return compareContainers(vector, stdvector);
+			}
+		));
 
-		std::cout << "Insert range to end(): ";
-		{
-			vector.insert(vector.end(), testArray, testArray + 3);
-			stdvector.insert(stdvector.end(), testArray, testArray + 3);
+		CHECK_RESULT(doTestCase(
+			"insert range to end()"
+			, [&]()
+			{
+				vector.insert(vector.end(), testArray, testArray + countObjects);
+				stdvector.insert(stdvector.end(), testArray, testArray + countObjects);
 
-			COMPARE_VECTORS(vector, stdvector)
-		}
+				return compareContainers(vector, stdvector);
+			}
+		));
 
-		std::cout << "Erase from end(): ";
-		{
-			vector.erase(vector.end() - 3, vector.end());
-			stdvector.erase(stdvector.end() - 3, stdvector.end());
+		CHECK_RESULT(doTestCase(
+			"erase from end()"
+			, [&]()
+			{
+				vector.erase(vector.end() - countObjects, vector.end());
+				stdvector.erase(stdvector.end() - countObjects, stdvector.end());
 
-			COMPARE_VECTORS(vector, stdvector)
-		}
+				return compareContainers(vector, stdvector);
+			}
+		));
 	}
 
 	return 0;
