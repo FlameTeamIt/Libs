cc6ce4561497cabb7c3abbccd22227f2738f5dcd
Anton Kashcheev <antonkachsheev@gmail.com>
2018-10-16 20:54:02 +0300

    Refactoring Allocator tests.

--- src/Templates/Tests/Allocator.cpp
+++ src/Templates/Tests/Allocator.cpp
@@ -18,38 +18,42 @@ int ObjectAllocator::vStart()
 	TestClass mainObject{ 1000, 10, 1, '1' };
 	TestClass *object;
 
-	std::cout << "Constructing object: ";
-	{
-		object = allocator.construct(
-				mainObject.getLong()
-				, mainObject.getInt()
-				, mainObject.getShort()
-				, mainObject.getChar()
-		);
-		if (object && *object == mainObject)
-		{
-			std::cout << "Success" << std::endl;
-		}
-		else
-		{
-			std::cout << "Failed" << std::endl;
-			return -1;
-		}
-	}
+	CHECK_RESULT(doTestCase(
+			"Constructing object"
+			, [&]()
+			{
+				object = allocator.construct(
+						mainObject.getLong()
+						, mainObject.getInt()
+						, mainObject.getShort()
+						, mainObject.getChar()
+				);
+				if(object && *object == mainObject)
+				{
+					return ResultType::SUCCESS;
+				}
+				else
+				{
+					return ResultType::FAILED;
+				}
+			}
+	));
 
-	std::cout << "Destructing object: ";
-	{
-		allocator.destroy(object);
-		if (!object)
-		{
-			std::cout << "Success" << std::endl;
-		}
-		else
-		{
-			std::cout << "Failed" << std::endl;
-			return -1;
-		}
-	}
+	CHECK_RESULT(doTestCase(
+			"Destructing object"
+			, [&]()
+			{
+				allocator.destroy(object);
+				if (!object)
+				{
+					return ResultType::SUCCESS;
+				}
+				else
+				{
+					return ResultType::FAILED;
+				}
+			}
+	));
 
 	return 0;
 }
@@ -64,101 +68,111 @@ ArrayAllocator::~ArrayAllocator()
 
 int ArrayAllocator::vStart()
 {
-	auto lambdaPrint = [] (const TestClass &testObject) {
-		std::cout << "{ "
-				<< "long("<< testObject.getLong() << "), "
-				<< "int(" << testObject.getInt() << "), "
-				<< "short(" << testObject.getShort() << "), "
-				<< "char('" << testObject.getChar() << "') "
-				<< "}";
-	};
+	struct
+	{
+		int current()
+		{
+			return index;
+		}
+
+		auto &add()
+		{
+			++index;
+			return *this;
+		}
+
+		int index = 0;
+	} idx;
 
 	using SsizeType = flame_ide::SizeTraits::SsizeType;
-	int index = 0;
 	constexpr SsizeType SIZE = 6;
-	TestClass array[6] = {
+	TestClass array[SIZE] = {
 			TestClass{
-					long((index += 1) * 1000), int(index * 100)
-					, short(10 + index), char('1' + index)
+					long(idx.add().current() * 1000), int(idx.current() * 100)
+					, short(idx.current() + 10), char(idx.current() + '1')
 			}
 			, TestClass{
-					long((index += 1) * 1000), int(index * 100)
-					, short(10 + index), char('1' + index)
+					long((idx.add().current()) * 1000), int(idx.current() * 100)
+					, short(idx.current() + 10), char(idx.current() + '1')
 			}
 			, TestClass{
-					long((index += 1) * 1000), int(index * 100)
-					, short(10 + index), char('1' + index)
+					long((idx.add().current()) * 1000), int(idx.current() * 100)
+					, short(idx.current() + 10), char(idx.current() + '1')
 			}
 			, TestClass{
-					long((index += 1) * 1000), int(index * 100)
-					, short(10 + index), char('1' + index)
+					long((idx.add().current()) * 1000), int(idx.current() * 100)
+					, short(idx.current() + 10), char(idx.current() + '1')
 			}
 			, TestClass{
-					long((index += 1) * 1000), int(index * 100)
-					, short(10 + index), char('1' + index)
+					long((idx.add().current()) * 1000), int(idx.current() * 100)
+					, short(idx.current() + 10), char(idx.current() + '1')
 			}
 			, TestClass{
-					long((index += 1) * 1000), int(index * 100)
-					, short(10 + index), char('1' + index)
+					long((idx.add().current()) * 1000), int(idx.current() * 100)
+					, short(idx.current() + 10), char(idx.current() + '1')
 			}
 	};
+	TestClass *testArray;
 
-	TestClass *testArray = allocator.construct(SIZE);
-	std::cout << "Check array pointer: ";
-	if (!testArray)
-	{
-		std::cout << "Failed." << std::endl;
-		return -1;
-	}
-	else
-	{
-		std::cout << "Success." << std::endl;
-	}
-
-	for (SsizeType i = 0; i < SIZE; ++i)
-	{
-		testArray[i] = {
-				array[i].getLong(), array[i].getInt()
-				, array[i].getShort(), array[i].getChar()
-		};
-	}
-
-	std::cout << "Check intializing: ";
-	{
-		for (SsizeType i = 0; i < SIZE; ++i)
-		{
-			if (array[i] != testArray[i])
+	CHECK_RESULT(doTestCase(
+			"Check array pointer"
+			, [&]()
 			{
-				std::cout << "Failed." << std::endl;
-				std::cout << "array[" << i << "]:" << std::endl;
-				lambdaPrint(array[i]);
-				std::cout << std::endl
-						<< "testArray[" << i << "]:" << std::endl;
-				lambdaPrint(testArray[i]);
-				std::cout << std::endl;
-
-				return -1;
+				testArray = allocator.construct(SIZE);
+				if (!testArray)
+				{
+					return ResultType::FAILED;
+				}
+				else
+				{
+					return ResultType::SUCCESS;
+				}
 			}
-		}
-		std::cout << "Success." << std::endl;
-	}
+	));
 
-	std::cout << "Destroying array: ";
-	allocator.destroy(testArray, SIZE);
-	{
-		if (testArray)
-		{
-			std::cout << "Failed." << std::endl;
-			return -1;
-		}
-		else
-		{
-			std::cout << "Success." << std::endl;
-		}
-	}
+	CHECK_RESULT(doTestCase(
+			"Check intializing"
+			, [&]()
+			{
+				for (SsizeType i = 0; i < SIZE; ++i)
+				{
+					testArray[i] = {
+							array[i].getLong(), array[i].getInt()
+							, array[i].getShort(), array[i].getChar()
+					};
+				}
+
+				for (SsizeType i = 0; i < SIZE; ++i)
+				{
+					if (array[i] != testArray[i])
+					{
+						return ResultType::FAILED;
+					}
+				}
+
+				return ResultType::SUCCESS;
+			}
+	));
+
+	CHECK_RESULT(doTestCase(
+			"Destroying array"
+			, [&]()
+			{
+				allocator.destroy(testArray, SIZE);
+				{
+					if (testArray)
+					{
+						return ResultType::FAILED;
+					}
+					else
+					{
+						return ResultType::SUCCESS;
+					}
+				}
+			}
+	));
 
 	return 0;
 }
 
 }}
-
