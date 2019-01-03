514a93b8250bcba37f36f27d3dee93689b46f228
Anton Kashcheev <antonkachsheev@gmail.com>
2019-01-03 08:02:34 +0300

    Continue implementation of CircularIterator.

--- include/FlameIDE/Templates/Iterator/CircularIterator.hpp
+++ include/FlameIDE/Templates/Iterator/CircularIterator.hpp
@@ -2,7 +2,11 @@
 #define FLAMEIDE_TEMPLATES_ITERATOR_CIRCULARITERATOR_HPP
 
 #include <FlameIDE/Templates/Iterator/IteratorTraits.hpp>
-#include <FlameIDE/Templates/Iterator/BaseIterator.hpp>
+#include <FlameIDE/Templates/Iterator/Iterator.hpp>
+
+#include <FlameIDE/Templates/Range.hpp>
+
+#include <FlameIDE/Templates/Iterator/Operators.hpp>
 
 namespace flame_ide
 {namespace templates
@@ -29,13 +33,35 @@ template<
 	, typename MetaType
 >
 class CircularIterator<IteratorType, IteratorCategory::FORWARD, Traits, MetaType>:
-		public iterator_utils::BaseIterator<
-			IteratorType, IteratorCategory::FORWARD, IteratorAccess::NON_CONSTANT
-			, Traits, MetaType
+		public Iterator<
+			IteratorType, IteratorCategory::FORWARD, Traits, MetaType
 		>
 {
 public:
+	using Parent = Iterator<
+		IteratorType, IteratorCategory::FORWARD, Traits, MetaType
+	>;
+	using Me = CircularIterator<
+		IteratorType, IteratorCategory::FORWARD, Traits, MetaType
+	>;
+	using Range = templates::Range<IteratorType>;
+
+	CircularIterator() = default;
+	CircularIterator(const Me &) = default;
+	CircularIterator(IteratorType iterator, Range containerActualRange)
+			: Parent(iterator)
+			, range(containerActualRange)
+	{}
+	~CircularIterator() = default;
+	Me& operator=(const Me &) = default;
+
+	FLAMEIDE_ITERATOR_OPERATOR_INC_POSTFIX
+
+	Me &operator++()
+	{}
+
 private:
+	mutable Range range;
 };
 
 template<
@@ -44,13 +70,38 @@ template<
 	, typename MetaType
 >
 class CircularIterator<IteratorType, IteratorCategory::BIDIRECTIONAL, Traits, MetaType>:
-		public iterator_utils::BaseIterator<
-			IteratorType, IteratorCategory::BIDIRECTIONAL, IteratorAccess::NON_CONSTANT
-			, Traits, MetaType
+		public Iterator<
+			IteratorType, IteratorCategory::BIDIRECTIONAL, Traits, MetaType
 		>
 {
-public:
+	using Parent = Iterator<
+		IteratorType, IteratorCategory::BIDIRECTIONAL, Traits, MetaType
+	>;
+	using Me = CircularIterator<
+		IteratorType, IteratorCategory::BIDIRECTIONAL, Traits, MetaType
+	>;
+	using Range = templates::Range<IteratorType>;
+
+	CircularIterator() = default;
+	CircularIterator(const Me &) = default;
+	CircularIterator(IteratorType iterator, Range containerActualRange)
+			: Parent(iterator)
+			, range(containerActualRange)
+	{}
+	~CircularIterator() = default;
+	Me& operator=(const Me &) = default;
+
+	FLAMEIDE_ITERATOR_OPERATOR_INC_POSTFIX
+	FLAMEIDE_ITERATOR_OPERATOR_DEC_POSTFIX
+
+	Me &operator++()
+	{}
+
+	Me &operator--()
+	{}
+
 private:
+	mutable Range range;
 };
 
 template<
@@ -59,15 +110,53 @@ template<
 	, typename MetaType
 >
 class CircularIterator<IteratorType, IteratorCategory::RANDOM_ACCESS, Traits, MetaType>:
-		public iterator_utils::BaseIterator<
-			IteratorType, IteratorCategory::RANDOM_ACCESS, IteratorAccess::NON_CONSTANT
-			, Traits, MetaType
+		public Iterator<
+			IteratorType, IteratorCategory::RANDOM_ACCESS, Traits, MetaType
 		>
 {
-public:
+	using Parent = Iterator<
+		IteratorType, IteratorCategory::RANDOM_ACCESS, Traits, MetaType
+	>;
+	using Me = CircularIterator<
+		IteratorType, IteratorCategory::RANDOM_ACCESS, Traits, MetaType
+	>;
+	using Range = templates::Range<IteratorType>;
+
+	CircularIterator() = default;
+	CircularIterator(const Me &) = default;
+	CircularIterator(IteratorType iterator, Range containerActualRange)
+			: Parent(iterator)
+			, range(containerActualRange)
+	{}
+	~CircularIterator() = default;
+	Me& operator=(const Me &) = default;
+
+	FLAMEIDE_ITERATOR_OPERATOR_INC_POSTFIX
+	FLAMEIDE_ITERATOR_OPERATOR_DEC_POSTFIX
+
+	FLAMEIDE_ITERATOR_OPERATOR_PLUS
+	FLAMEIDE_ITERATOR_OPERATOR_MINUS
+
+	Me &operator++()
+	{}
+
+	Me &operator--()
+	{}
+
+	template<typename IntType>
+	Me &operator+=(IntType value)
+	{}
+
+	template<typename IntType>
+	Me &operator-=(IntType value)
+	{}
+
 private:
+	mutable Range range;
 };
 
 }}
+
+#include <FlameIDE/Templates/Iterator/UndefOperators.hpp>
 
 #endif // FLAMEIDE_TEMPLATES_ITERATOR_CIRCULARITERATOR_HPP
