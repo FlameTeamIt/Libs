9fbf9c081e1bbfa12419cd60c85461d86795d3b8
Anton Kashcheev <antonkachsheev@gmail.com>
2019-01-12 03:51:19 +0300

    Add custom implementations of 'copy()', 'foreach()', 'foreachChangable()'.

--- include/FlameIDE/Templates/SimpleAlgorithms.hpp
+++ include/FlameIDE/Templates/SimpleAlgorithms.hpp
@@ -2,45 +2,60 @@
 #define FLAMEIDE_TEMPLATES_SIMPLEALGORITHMS_HPP
 
 #include <FlameIDE/Common/Utils.hpp>
+#include <FlameIDE/Templates/Range.hpp>
 
 namespace flame_ide
 {namespace templates
 {
 
-/**
- * @brief Copying range.
- */
+///
+/// @brief Copying range.
+///
 template<typename IteratorInput, typename IteratorOutput>
 Types::size_t copy(IteratorInput start, IteratorInput end
 		, IteratorOutput out);
-template<typename ContainerInput, typename IteratorOutput>
-Types::size_t copy(const ContainerInput &input, IteratorOutput out);
 
+template<typename InputContainer, typename IteratorOutput>
+Types::size_t copy(const InputContainer &input, IteratorOutput out);
 
-/**
- * @brief Getting size of range.
- */
+template<typename InputIterator, typename IteratorOutput>
+Types::size_t copy(const Range<InputIterator> &range, IteratorOutput out);
+
+///
+/// @brief Getting size of range.
+///
 template<typename Iterator, typename SizeTraits = flame_ide::SizeTraits>
 typename SizeTraits::SizeType countIterations(Iterator start, Iterator end);
 
-
-/**
- * @brief Iterate from start to end.
- */
+///
+/// @brief Iterate from start to end.
+///
 template<typename IteratorInput, typename FuncObject>
-void foreach(IteratorInput start, IteratorInput end, FuncObject func);
+void foreach(IteratorInput start, IteratorInput end, FuncObject &&func);
 
-/**
- * @brief Iterate by all container elements without changing.
- */
-template<typename ContainerInput, typename FuncObject>
-void foreach(const ContainerInput& container, FuncObject func);
+///
+/// @brief Iterate by all container elements without changing.
+///
+template<typename InputContainer, typename FuncObject>
+void foreach(const InputContainer &container, FuncObject &&func);
 
-/**
- * @brief Iterate by all container elements with changing.
- */
-template<typename ContainerInput, typename FuncObject>
-void foreachChangable(ContainerInput& container, FuncObject func);
+///
+/// @brief Iterate by range elements without changing.
+///
+template<typename InputIterator, typename FuncObject>
+void foreach(const Range<InputIterator> &range, FuncObject &&func);
+
+///
+/// @brief Iterate by all container elements with changing.
+///
+template<typename InputContainer, typename FuncObject>
+void foreachChangable(InputContainer &container, FuncObject &&func);
+
+///
+/// @brief Iterate by range elements with changing.
+///
+template<typename InputIterator, typename FuncObject>
+void foreachChangable(Range<InputIterator> &range, FuncObject &&func);
 
 }}
 
@@ -69,10 +84,16 @@ Types::size_t copy(IteratorInput start, IteratorInput end
 	return counter;
 }
 
-template<typename ContainerInput, typename IteratorOutput>
-Types::size_t copy(const ContainerInput &input, IteratorOutput out)
+template<typename InputContainer, typename IteratorOutput>
+Types::size_t copy(const InputContainer &input, IteratorOutput out)
 {
 	return copy(input.begin(), input.end(), out);
+}
+
+template<typename InputIterator, typename IteratorOutput>
+Types::size_t copy(const Range<InputIterator> &range, IteratorOutput out)
+{
+	return copy(range.begin(), range.end(), out);
 }
 
 template<typename Iterator, typename Traits>
@@ -84,9 +105,8 @@ typename Traits::SizeType countIterations(Iterator start, Iterator end)
 	return count;
 }
 
-
 template<typename IteratorInput, typename FuncObject>
-void foreach(IteratorInput start, IteratorInput end, FuncObject func)
+void foreach(IteratorInput start, IteratorInput end, FuncObject &&func)
 {
 	for (auto it = start; it != end; ++it)
 	{
@@ -94,16 +114,28 @@ void foreach(IteratorInput start, IteratorInput end, FuncObject func)
 	}
 }
 
-template<typename ContainerInput, typename FuncObject>
-void foreach(const ContainerInput& container, FuncObject func)
+template<typename InputContainer, typename FuncObject>
+void foreach(const InputContainer& container, FuncObject func)
 {
 	foreach(container.begin(), container.end(), func);
 }
 
-template<typename ContainerInput, typename FuncObject>
-void foreachChangable(ContainerInput& container, FuncObject func)
+template<typename InputIterator, typename FuncObject>
+void foreach(const Range<InputIterator> &range, FuncObject &&func)
+{
+	foreach(range.begin(), range.end(), func);
+}
+
+template<typename InputContainer, typename FuncObject>
+void foreachChangable(InputContainer& container, FuncObject &&func)
 {
 	foreach(container.begin(), container.end(), func);
+}
+
+template<typename InputIterator, typename FuncObject>
+void foreachChangable(Range<InputIterator> &range, FuncObject &&func)
+{
+	foreach(range.begin(), range.end(), func);
 }
 
 }}
