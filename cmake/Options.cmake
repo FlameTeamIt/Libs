c427e4fc026f699902671747eb21c33ac0a659ec
Anton Kashcheev <antonkachsheev@gmail.com>
2019-11-28 01:34:38 +0300

    Fix options.

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -1,4 +1,5 @@
 option(FLAME_CMAKE_DEBUG "" OFF)
+
 option(FLAME_ENABLE_ASYNC "Enabling async i/o" OFF)
 
 option(FLAME_CREATE_STATIC "Creating static libraries" ON)
@@ -6,4 +7,4 @@ option(FLAME_CREATE_SHARED "Creating shared libraries" ON)
 
 option(FLAME_MAKE_STANDALONE "Make all modules as standalone" OFF)
 
-option(FLAME_SHOW_HEADERS_AS_TARGET "Show headers as costom tsrget in Qt Creator" OFF)
+option(FLAME_SHOW_HEADERS_AS_TARGET "Show headers as custom target in Qt Creator" OFF)
