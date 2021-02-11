f7dce99759c47ff8c68acc3051183ab3aff5cdc0
Anton Kashcheev <antonkachsheev@gmail.com>
2021-02-11 04:17:46 +0300

    Fix reaction after finding git; fix 'download_resolver()' function

--- cmake/FlameContrib.cmake
+++ cmake/FlameContrib.cmake
@@ -2,16 +2,17 @@ cmake_minimum_required(VERSION 3.14)
 
 find_package(Git)
 if(NOT Git_FOUND)
-	message(FATAL "Git not found.")
+	message(FATAL_ERROR "Git not found.")
 endif()
 
 function(download_resolver)
 	set(RESOLVER_NAME CMakeFlameResolver)
 	set(RESOLVER_PATH ${CMAKE_CURRENT_SOURCE_DIR}/cmake/${RESOLVER_NAME})
+	set(RESOLVER_URL "https://github.com/kachsheev/${RESOLVER_NAME}.git")
+	set(RESOLVER_VERSION "v1.0")
 	if(NOT (EXISTS "${RESOLVER_PATH}" AND IS_DIRECTORY "${RESOLVER_PATH}"))
-		execute_process(COMMAND ${GIT_EXECUTABLE}
-			clone https://github.com/kachsheev/${RESOLVER_NAME}.git
-			--branch v1.0
+		execute_process(COMMAND
+			${GIT_EXECUTABLE} clone ${RESOLVER_URL} --branch ${RESOLVER_VERSION}
 			${RESOLVER_PATH}
 		)
 	endif()
