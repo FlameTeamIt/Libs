ed6809650cfb442d70407658e3b14d53d4bf0ac8
Anton Kashcheev <antonkachsheev@gmail.com>
2021-02-20 02:48:17 +0300

    include/FlameIDE/Common/Macros*: fix macro guard name

--- include/FlameIDE/Common/Macros.hpp
+++ include/FlameIDE/Common/Macros.hpp
@@ -1,7 +1,7 @@
-﻿#ifndef FLAMEIDE_COMMON_MACROSES_HPP
-#define FLAMEIDE_COMMON_MACROSES_HPP
+﻿#ifndef FLAMEIDE_COMMON_MACROS_HPP
+#define FLAMEIDE_COMMON_MACROS_HPP
 
 #include <FlameIDE/Common/Macros/DetectOs.hpp>
 #include <FlameIDE/Common/Macros/ByteOrder.hpp>
 
-#endif // FLAMEIDE_COMMON_MACROSES_HPP
+#endif // FLAMEIDE_COMMON_MACROS_HPP
