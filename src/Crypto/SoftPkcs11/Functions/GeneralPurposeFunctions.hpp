599d236ca768c9d67b9b5b4d97c30796847f2f57
Anton Kashcheev <antonkachsheev@gmail.com>
2021-02-21 01:12:37 +0300

    src/Crypto/SoftPkcs11/Functions/GeneralPurposeFunctions.hpp: fix comments

--- src/Crypto/SoftPkcs11/Functions/GeneralPurposeFunctions.hpp
+++ src/Crypto/SoftPkcs11/Functions/GeneralPurposeFunctions.hpp
@@ -11,33 +11,33 @@ namespace flame_ide
 {
 
 ///
-/// \brief initialize
-/// \param arguments
-/// \return
+/// @brief initialize
+/// @param arguments
+/// @return
 ///
 pkcs11::value_types::ReturnType initialize(pkcs11::value_types::ValuePtr arguments);
 
 ///
-/// \brief finalize
-/// \return
+/// @brief finalize
+/// @return
 ///
 pkcs11::value_types::ReturnType finalize(
 		pkcs11::value_types::ValuePtr /* reserved */
 );
 
 ///
-/// \brief getInfo
-/// \param info
-/// \return
+/// @brief getInfo
+/// @param info
+/// @return
 ///
 pkcs11::value_types::ReturnType getInfo(
 		pkcs11::structs::InfoPtr info
 );
 
 ///
-/// \brief getFunctionList
-/// \param functionList
-/// \return
+/// @brief getFunctionList
+/// @param functionList
+/// @return
 ///
 pkcs11::value_types::ReturnType getFunctionList(
 		pkcs11::structs::FunctionListPtrPtr functionList
