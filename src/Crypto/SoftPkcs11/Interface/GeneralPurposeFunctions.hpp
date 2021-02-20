88d169fee7430ce8c5cecfbac2d47379a28b1dd8
Anton Kashcheev <antonkachsheev@gmail.com>
2021-02-21 01:12:07 +0300

    Add documentation to Interface/GeneralPurposeFunctions.hpp

--- src/Crypto/SoftPkcs11/Interface/GeneralPurposeFunctions.hpp
+++ src/Crypto/SoftPkcs11/Interface/GeneralPurposeFunctions.hpp
@@ -8,12 +8,33 @@ extern "C"
 {
 #endif
 
+///
+/// @brief Initializes the Cryptoki library
+/// @param[in] pInitArgs if this is not NULL_PTR, it gets cast to
+/// CK_C_INITIALIZE_ARGS_PTR and dereferenced
+/// @return See CKR_* values
+///
 CK_RV C_Initialize(CK_VOID_PTR pInitArgs);
 
+///
+/// @brief Indicates that an application is done with the Cryptoki library
+/// @param pReserved reserved; should be NULL_PTR
+/// @return
+///
 CK_RV C_Finalize(CK_VOID_PTR pReserved);
 
+///
+/// @brief Returns general information about Cryptoki
+/// @param[out] pInfo Location that receives information
+/// @return See CKR_* values
+///
 CK_RV C_GetInfo(CK_INFO_PTR pInfo);
 
+///
+/// @brief Returns the function list
+/// @param ppFunctionList receives pointer to function list
+/// @return See CKR_* values
+///
 CK_RV C_GetFunctionList(CK_FUNCTION_LIST_PTR_PTR ppFunctionList);
 
 #ifdef __cplusplus
