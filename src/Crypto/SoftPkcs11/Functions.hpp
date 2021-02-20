04be7e47b5c40f45a71ea683db8abad5e02699e2
Anton Kashcheev <antonkachsheev@gmail.com>
2021-02-20 16:39:04 +0300

    src/Crypto/SoftPkcs11/Functions.hpp: refactor header list

--- src/Crypto/SoftPkcs11/Functions.hpp
+++ src/Crypto/SoftPkcs11/Functions.hpp
@@ -1,18 +1,41 @@
 #ifndef FLAMEIDE_SRC_CRYPTO_SOFTPKCS11_FUNCTIONS_HPP
 #define FLAMEIDE_SRC_CRYPTO_SOFTPKCS11_FUNCTIONS_HPP
 
-#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/DecryptFunctions.hpp>
-#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/DigestFunctions.hpp>
-#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/DualOperationFunctions.hpp>
-#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/EncryptFunctions.hpp>
+/* General-purpose */
 #include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/GeneralPurposeFunctions.hpp>
-#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/KeyFunctions.hpp>
+
+/* Slot and token management */
 #include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/ManagementFunctions.hpp>
-#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/ObjectFunctions.hpp>
-#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/OtherFunctions.hpp>
-#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/ParallelManagementFunctions.hpp>
+
+/* Session management */
 #include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/SessionFunctions.hpp>
+
+/* Object management */
+#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/ObjectFunctions.hpp>
+
+/* Encryption */
+#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/EncryptFunctions.hpp>
+
+/* Decryption */
+#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/DecryptFunctions.hpp>
+
+/* Message digesting */
+#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/DigestFunctions.hpp>
+
+/* Signing and MACing */
 #include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/SignFunctions.hpp>
+
+/* Verifying signatures and MACs */
 #include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/VerifyFunctions.hpp>
+
+/* Dual-function cryptographic operations */
+#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/DualOperationFunctions.hpp>
+
+/* Key management */
+#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/KeyFunctions.hpp>
+#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/OtherFunctions.hpp>
+
+/* Parallel function management */
+#include <FlameIDE/../../src/Crypto/SoftPkcs11/Functions/ParallelManagementFunctions.hpp>
 
 #endif // FLAMEIDE_SRC_CRYPTO_SOFTPKCS11_FUNCTIONS_HPP
