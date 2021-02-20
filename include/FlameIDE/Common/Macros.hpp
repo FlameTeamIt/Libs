c3baf2cd1f24a5f1f9f3eb66ebf31b7a4a952c8c
Anton Kashcheev <antonkachsheev@gmail.com>
2021-02-20 16:30:53 +0300

    Add header for detecting compilers

--- include/FlameIDE/Common/Macros.hpp
+++ include/FlameIDE/Common/Macros.hpp
@@ -2,6 +2,7 @@
 #define FLAMEIDE_COMMON_MACROS_HPP
 
 #include <FlameIDE/Common/Macros/DetectOs.hpp>
+#include <FlameIDE/Common/Macros/DetectCompiler.hpp>
 #include <FlameIDE/Common/Macros/ByteOrder.hpp>
 
 #endif // FLAMEIDE_COMMON_MACROS_HPP
