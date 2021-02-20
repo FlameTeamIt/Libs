959f4b76cb16c54f1c1ba1f9c8671ca1f8616950
Anton Kashcheev <antonkachsheev@gmail.com>
2021-02-21 01:39:13 +0300

    Add documentation for interface management functions

--- src/Crypto/SoftPkcs11/Interface/ManagementFunctions.hpp
+++ src/Crypto/SoftPkcs11/Interface/ManagementFunctions.hpp
@@ -9,11 +9,11 @@ extern "C"
 #endif
 
 ///
-/// @brief C_GetSlotList
-/// @param tokenPresent
-/// @param pSlotList
-/// @param pulCount
-/// @return
+/// @brief Obtains a list of slots in the system
+/// @param[in] tokenPresent Only slots with tokens
+/// @param[out] pSlotList Receives array of slot IDs
+/// @param[out] pulCount Receives number of slots
+/// @return See CKR_* values
 ///
 CK_RV C_GetSlotList(
 		CK_BBOOL tokenPresent
@@ -22,10 +22,10 @@ CK_RV C_GetSlotList(
 );
 
 ///
-/// @brief C_GetSlotInfo
-/// @param slotId
-/// @param pInfo
-/// @return
+/// @brief Obtains information about a particular slot in the system
+/// @param[in] slotId The ID of the slot
+/// @param[out] pInfo Receives the slot information
+/// @return See CKR_* values
 ///
 CK_RV C_GetSlotInfo(
 		CK_SLOT_ID slotId
@@ -33,86 +33,65 @@ CK_RV C_GetSlotInfo(
 );
 
 ///
-/// @brief C_GetTokenInfo
-/// @param slotId
-/// @param pInfo
-/// @return
+/// @brief Obtains information about a particular token
+/// @param[in] slotId ID of the token's slot
+/// @param[out] pInfo receives the token information
+/// @return See CKR_* values
 ///
-CK_RV C_GetTokenInfo(
-		CK_SLOT_ID slotId
-		, CK_TOKEN_INFO_PTR pInfo
-);
+CK_RV C_GetTokenInfo(CK_SLOT_ID slotId, CK_TOKEN_INFO_PTR pInfo);
 
 ///
-/// @brief C_GetMechanismList
-/// @param slotId
-/// @param pMechanismList
-/// @param pulCount
-/// @return
+/// @brief Obtains a list of mechanism types supported by a token
+/// @param[in] slotId ID of token's slot
+/// @param[out] pMechanismList Gets mechanism array
+/// @param[out] pulCount Gets count of mechanisms
+/// @return See CKR_* values
 ///
-CK_RV C_GetMechanismList(
-		CK_SLOT_ID slotId
-		, CK_MECHANISM_TYPE_PTR pMechanismList
-		, CK_ULONG_PTR pulCount
-);
+CK_RV C_GetMechanismList(CK_SLOT_ID slotId, CK_MECHANISM_TYPE_PTR pMechanismList
+		, CK_ULONG_PTR pulCount);
 
 ///
-/// @brief C_GetMechanismInfo
-/// @param slotId
-/// @param type
-/// @param pInfo
-/// @return
+/// @brief Obtains information about a particular mechanism possibly
+/// supported by a token
+/// @param[in] slotId ID of the token's slot
+/// @param[in] type Type of mechanism
+/// @param[out] pInfo Receives mechanism info
+/// @return See CKR_* values
 ///
-CK_RV C_GetMechanismInfo(
-		CK_SLOT_ID slotId
-		, CK_MECHANISM_TYPE type
-		, CK_MECHANISM_INFO_PTR pInfo
-);
+CK_RV C_GetMechanismInfo(CK_SLOT_ID slotId, CK_MECHANISM_TYPE type
+		, CK_MECHANISM_INFO_PTR pInfo);
 
 ///
-/// @brief C_InitToken
-/// @param slotId
-/// @param pPin
-/// @param ulPinLen
-/// @param pLabel
-/// @return
+/// @brief Initializes a token
+/// @param[in] slotId ID of the token's slot
+/// @param[in] pPin The SO's initial PIN
+/// @param[in] ulPinLen Length in bytes of the PIN
+/// @param[in] pLabel 32-byte token label (blank padded)
+/// @return See CKR_* values
 ///
-CK_RV C_InitToken(
-		CK_SLOT_ID slotId
-		, CK_UTF8CHAR_PTR pPin
-		, CK_ULONG ulPinLen
-		, CK_UTF8CHAR_PTR pLabel
-);
+CK_RV C_InitToken(CK_SLOT_ID slotId, CK_UTF8CHAR_PTR pPin, CK_ULONG ulPinLen
+		, CK_UTF8CHAR_PTR pLabel);
 
 ///
-/// @brief C_InitPIN
-/// @param hSession
-/// @param pPin
-/// @param ulPinLen
-/// @return
+/// @brief Initializes the normal user's PIN
+/// @param[in] hSession The session's handle
+/// @param[in] pPin The normal user's PIN
+/// @param[in] ulPinLen Length in bytes of the PIN
+/// @return See CKR_* values
 ///
-CK_RV C_InitPIN(
-		CK_SESSION_HANDLE hSession
-		, CK_UTF8CHAR_PTR pPin
-		, CK_ULONG ulPinLen
-);
+CK_RV C_InitPIN(CK_SESSION_HANDLE hSession, CK_UTF8CHAR_PTR pPin, CK_ULONG ulPinLen);
 
 ///
-/// @brief C_SetPIN
-/// @param hSession
-/// @param pOldPin
-/// @param ulOldLen
-/// @param pNewPin
-/// @param ulNewLen
-/// @return
+/// @brief Modifies the PIN of the user who is logged in
+/// @param[in] hSession The session's handle
+/// @param[in] pOldPin The old PIN
+/// @param[in] ulOldLen Length of the old PIN
+/// @param[in] pNewPin The new PIN
+/// @param[in] ulNewLen Length of the new PIN
+/// @return See CKR_* values
 ///
-CK_RV C_SetPIN(
-		CK_SESSION_HANDLE hSession
-		, CK_UTF8CHAR_PTR pOldPin
-		, CK_ULONG ulOldLen
-		, CK_UTF8CHAR_PTR pNewPin
-		, CK_ULONG ulNewLen
-);
+CK_RV C_SetPIN(CK_SESSION_HANDLE hSession, CK_UTF8CHAR_PTR pOldPin, CK_ULONG ulOldLen
+		, CK_UTF8CHAR_PTR pNewPin, CK_ULONG ulNewLen);
 
 #ifdef __cplusplus
 }
