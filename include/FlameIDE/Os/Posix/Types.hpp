dffbc679ca6c2fa0d9c0d23fb388f3131ee3e889
Anton Kashcheev <antonkachsheev@gmail.com>
2021-02-23 22:58:32 +0300

    Add Os/Types - copied from Common

---
+++ include/FlameIDE/Os/Posix/Types.hpp
@@ -0,0 +1,106 @@
+#ifndef FLAMEIDE_OS_POSIX_TYPES_HPP
+#define FLAMEIDE_OS_POSIX_TYPES_HPP
+
+#include <FlameIDE/Common/Macros/DetectOs.hpp>
+
+#if FLAMEIDE_OS_POSIX != FLAMEIDE_OS_NULL
+
+#include <FlameIDE/Common/Traits/CreationProhibitions.hpp>
+#include <FlameIDE/Common/Traits/Numbers.hpp>
+
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+namespace flame_ide
+{namespace os
+{namespace posix
+{
+
+class OsFileDescriptor
+{
+public:
+	using DefaultFd = int;
+
+	constexpr OsFileDescriptor() : OsFileDescriptor(DefaultFd{-1})
+	{}
+
+	constexpr OsFileDescriptor(DefaultFd fd) : defaultFd(fd)
+	{}
+
+	constexpr operator int() noexcept
+	{
+		return defaultFd;
+	}
+
+	constexpr operator const int() const noexcept
+	{
+		return defaultFd;
+	}
+
+	constexpr bool operator==(OsFileDescriptor fd) const noexcept
+	{
+		return defaultFd == fd.defaultFd;
+	}
+
+	constexpr bool operator!=(OsFileDescriptor fd) const noexcept
+	{
+		return !(operator==(fd));
+	}
+
+private:
+	DefaultFd defaultFd;
+};
+constexpr OsFileDescriptor OS_INVALID_DESCRIPTOR = -1;
+
+using OsStatus = int;
+constexpr OsStatus OS_STATUS_SUCCESS = 0;
+
+struct OsSocket
+{
+	::sockaddr_un sockaddr;
+	OsFileDescriptor fd;
+};
+constexpr OsSocket OS_SOCKET_INITIALIZER = OsSocket {
+		{}, OS_INVALID_DESCRIPTOR
+};
+
+struct OsThreadContext
+{
+	pthread_t object;
+	pthread_attr_t attributes;
+};
+constexpr OsThreadContext OS_THREAD_CONTEXT_INITIALIZER = OsThreadContext{};
+struct OsThreadTaskTrait : NonCreational
+{
+	using ReturnType = void *;
+	using ArgumentType = void *;
+	using TaskType = ReturnType(*)(ArgumentType);
+};
+
+struct OsMutexContext
+{
+	pthread_mutex_t object;
+	pthread_mutexattr_t attributes;
+};
+constexpr OsMutexContext OS_MUTEX_CONTEXT_INITIALIZER = {
+	PTHREAD_MUTEX_INITIALIZER, {}
+};
+
+using OsSemaphoreContext = ::sem_t;
+constexpr OsSemaphoreContext OS_SEMAPHORE_CONTEXT_INITIALIZER = OsSemaphoreContext{};
+using OsSemaphoreValue = unsigned int;
+constexpr OsSemaphoreValue OS_SEMAPHORE_VALUE_DEFAULT = 1u;
+constexpr OsSemaphoreValue OS_SEMAPHORE_VALUE_INVALID =
+		NumberLimitTrait<OsSemaphoreValue>::MAX_VALUE;
+
+constexpr char OS_PLATFORM_PREFIX[] = "/tmp/";
+
+}}} // flame_ide::os::posix
+
+#define FLAMEIDE_OS_NAMESPACE flame_ide::os::posix
+
+#endif // FLAMEIDE_OS_POSIX != FLAMEIDE_OS_NULL
+
+#endif // FLAMEIDE_OS_POSIX_TYPES_HPP
---
+++ include/FlameIDE/Os/Types.hpp
@@ -0,0 +1,96 @@
+#ifndef FLAMEIDE_OS_TYPES_HPP
+#define FLAMEIDE_OS_TYPES_HPP
+
+#include <FlameIDE/Os/Posix/Types.hpp>
+#include <FlameIDE/Os/Windows/Types.hpp>
+
+namespace flame_ide
+{namespace os
+{
+
+/// @brief
+using FileDescriptor = FLAMEIDE_OS_NAMESPACE::OsFileDescriptor;
+
+/// @brief
+constexpr FileDescriptor INVALID_DESCRIPTOR =
+		FLAMEIDE_OS_NAMESPACE::OS_INVALID_DESCRIPTOR;
+
+/// @brief
+using Status = FLAMEIDE_OS_NAMESPACE::OsStatus;
+
+/// @brief
+constexpr Status STATUS_SUCCESS =
+		FLAMEIDE_OS_NAMESPACE::OS_STATUS_SUCCESS;
+
+/// @brief
+using Socket = FLAMEIDE_OS_NAMESPACE::OsSocket;
+constexpr Socket SOCKET_INITIALIZER =
+		FLAMEIDE_OS_NAMESPACE::OS_SOCKET_INITIALIZER;
+
+/// @brief
+using ThreadContext = FLAMEIDE_OS_NAMESPACE::OsThreadContext;
+
+/// @brief
+constexpr ThreadContext THREAD_CONTEXT_INITIALIZER =
+		FLAMEIDE_OS_NAMESPACE::OS_THREAD_CONTEXT_INITIALIZER;
+
+/// @brief
+using ThreadTaskTrait = FLAMEIDE_OS_NAMESPACE::OsThreadTaskTrait;
+
+/// @brief
+using MutexContext = FLAMEIDE_OS_NAMESPACE::OsMutexContext;
+
+/// @brief
+constexpr MutexContext MUTEX_CONTEXT_INITIALIZER =
+		FLAMEIDE_OS_NAMESPACE::OS_MUTEX_CONTEXT_INITIALIZER;
+
+/// @brief
+using SemaphoreContext = FLAMEIDE_OS_NAMESPACE::OsSemaphoreContext;
+
+/// @brief
+constexpr SemaphoreContext SEMAPHORE_CONTEXT_INITIALIZER =
+		FLAMEIDE_OS_NAMESPACE::OS_SEMAPHORE_CONTEXT_INITIALIZER;
+
+/// @brief
+using SemaphoreValue = FLAMEIDE_OS_NAMESPACE::OsSemaphoreValue;
+
+/// @brief
+constexpr SemaphoreValue SEMAPHORE_VALUE_DEFAULT =
+		FLAMEIDE_OS_NAMESPACE::OS_SEMAPHORE_VALUE_DEFAULT;
+
+/// @brief
+constexpr SemaphoreValue SEMAPHORE_VALUE_INVALID =
+		FLAMEIDE_OS_NAMESPACE::OS_SEMAPHORE_VALUE_INVALID;
+
+/// @brief The ActionType enum
+enum class ActionType
+{
+	READER
+	, WRITER
+	, BIDIRECTIONAL
+};
+
+/// @brief The ActionMethod enum
+enum class ActionMethod
+{
+	SYNCHRONOUS
+	, ASYNCHRONOUS
+};
+
+/// @brief The StreamType enum
+enum class StreamType
+{
+	MEMORY
+	, PIPE
+	, NAMED_PIPE
+	, FILE
+	, SOCKET
+};
+
+/// @brief PLATFORM_PREFIX
+constexpr auto PIPE_NAME_PREFIX = FLAMEIDE_OS_NAMESPACE::OS_PLATFORM_PREFIX;
+
+
+}} // namespace flame_ide::os
+
+#endif // FLAMEIDE_OS_TYPES_HPP
