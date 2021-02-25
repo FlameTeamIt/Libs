17831b774c2d51da0b5c2a0d5d904db6703421a6
Anton Kashcheev <antonkachsheev@gmail.com>
2021-02-25 03:16:56 +0300

    Update ReferenceWrapper

--- include/FlameIDE/Common/ReferenceWrapper.hpp
+++ include/FlameIDE/Common/ReferenceWrapper.hpp
@@ -1,52 +1,97 @@
 #ifndef FLAMEIDE_COMMON_REFERENCE_WRAPPER_HPP
 #define FLAMEIDE_COMMON_REFERENCE_WRAPPER_HPP
 
+#include <FlameIDE/Common/Traits/ContainerTraits.hpp>
+
 namespace flame_ide
 {
 
 ///
 /// @brief The ReferenceWrapper class
 ///
-template<typename T>
+template<typename T, typename Traits = ContainerTraits<T>>
 class ReferenceWrapper
 {
 public:
-	inline ReferenceWrapper(T &initValue) noexcept : value(&initValue)
-	{}
+	using Pointer = typename Traits::Pointer;
+	using Reference = typename Traits::Reference;
 
-	T &get() const
-	{
-		return *value;
-	}
+	///
+	/// @brief ReferenceWrapper
+	/// @param initValue
+	///
+	ReferenceWrapper(Pointer initValue) noexcept;
+
+	///
+	/// @brief ReferenceWrapper
+	/// @param initValue
+	///
+	ReferenceWrapper(Reference initValue) noexcept;
+
+	///
+	/// @brief get
+	/// @return
+	///
+	Reference get() const noexcept;
+
+	///
+	/// @brief operator bool
+	///
+	operator bool() const noexcept;
 
 private:
-	mutable T *value;
-};
-
-///
-/// @brief The ReferenceWrapper<const T> class
-///
-template<typename T>
-class ReferenceWrapper<const T>
-{
-public:
-	inline ReferenceWrapper(const T &initValue) noexcept : value(&initValue)
-	{}
-
-	const T &get() const
-	{
-		return *value;
-	}
-
-private:
-	mutable const T *value;
+	mutable Pointer value; ///<
 };
 
 ///
 /// @brief the ConstReferenceWrapper alias
 ///
 template<typename T>
-using ConstReferenceWrapper = ReferenceWrapper<const T>;
+using ConstReferenceWrapper = ReferenceWrapper<const T, ConstTraits<T>>;
+
+template<typename T>
+ReferenceWrapper<T> makeReferenceWrapper(T &value) noexcept;
+
+template<typename T>
+ConstReferenceWrapper<T> makeConstReferenceWrapper(T &value) noexcept;
+
+template<typename T>
+T &getReference(T &value) noexcept;
+
+template<typename T>
+const T &getConstReference(const T &value);
+
+} // namespace flame_ide
+
+namespace flame_ide
+{
+
+// ReferenceWrapper
+
+template<typename T, typename Traits>
+ReferenceWrapper<T, Traits>::ReferenceWrapper(Pointer initValue) noexcept :
+		value(initValue)
+{}
+
+template<typename T, typename Traits>
+ReferenceWrapper<T, Traits>::ReferenceWrapper(Reference initValue) noexcept :
+		value(&initValue)
+{}
+
+template<typename T, typename Traits>
+typename ReferenceWrapper<T, Traits>::Reference
+ReferenceWrapper<T, Traits>::get() const noexcept
+{
+	return *value;
+}
+
+template<typename T, typename Traits>
+ReferenceWrapper<T, Traits>::operator bool() const noexcept
+{
+	return value != nullptr;
+}
+
+// functions
 
 template<typename T>
 ReferenceWrapper<T> makeReferenceWrapper(T &value) noexcept
@@ -61,15 +106,15 @@ ConstReferenceWrapper<T> makeConstReferenceWrapper(T &value) noexcept
 }
 
 template<typename T>
-inline T &getReference(T &value) noexcept
+T &getReference(T &value) noexcept
 {
-	return value;
+	return makeReferenceWrapper(value).get();
 }
 
 template<typename T>
 const T &getConstReference(const T &value)
 {
-	return value;
+	return makeConstReferenceWrapper(value).get();
 }
 
 } // namespace flame_ide
