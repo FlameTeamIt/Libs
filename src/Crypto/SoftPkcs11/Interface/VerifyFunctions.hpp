1a3767ca7ea022012187fc50727f5ad1dfbeb5d4
Anton Kashcheev <antonkachsheev@gmail.com>
2021-02-25 21:57:38 +0300

    Add documentation to Interface/VerfyFunctions.hpp

--- src/Crypto/SoftPkcs11/Interface/VerifyFunctions.hpp
+++ src/Crypto/SoftPkcs11/Interface/VerifyFunctions.hpp
@@ -9,90 +9,73 @@ extern "C"
 #endif
 
 ///
-/// @brief C_VerifyInit
-/// @param hSession
-/// @param pMechanism
-/// @param hKey
-/// @return
+/// @brief Initialize a verification operation, where the signature is an appendix
+/// to the data, and plaintext cannot be recovered from the signature (e.g. DSA)
+/// @param[in] hSession The session's handle
+/// @param[in] pMechanism The verification mechanism
+/// @param[in] hKey Verification key
+/// @return See CKR_* values
 ///
-CK_RV C_VerifyInit(
-		CK_SESSION_HANDLE hSession
-		, CK_MECHANISM_PTR pMechanism
-		, CK_OBJECT_HANDLE hKey
-);
+CK_RV C_VerifyInit(CK_SESSION_HANDLE hSession, CK_MECHANISM_PTR pMechanism
+		, CK_OBJECT_HANDLE hKey);
 
 ///
-/// @brief C_Verify
-/// @param hSession
-/// @param pData
-/// @param ulDataLen
-/// @param pSignature
-/// @param ulSignatureLen
-/// @return
+/// @brief Verify a signature in a single-part operation, where the signature is
+/// an appendix to the data, and plaintext cannot be recovered from the signature
+/// @param[in] hSession The session's handle
+/// @param[in] pData Signed data
+/// @param[in] ulDataLen Length of signed data
+/// @param[in] pSignature Signature
+/// @param[in] ulSignatureLen Signature length
+/// @return See CKR_* values
 ///
-CK_RV C_Verify(
-		CK_SESSION_HANDLE hSession
-		, CK_BYTE_PTR pData
-		, CK_ULONG ulDataLen
-		, CK_BYTE_PTR pSignature
-		, CK_ULONG ulSignatureLen
-);
+CK_RV C_Verify(CK_SESSION_HANDLE hSession, CK_BYTE_PTR pData, CK_ULONG ulDataLen
+		, CK_BYTE_PTR pSignature, CK_ULONG ulSignatureLen);
 
 ///
-/// @brief C_VerifyUpdate
-/// @param hSession
-/// @param pPart
-/// @param ulPartLen
-/// @return
+/// @brief Continue a multiple-part verification operation, where the signature is
+/// an appendix to the data, and plaintext cannot be recovered from the signature
+/// @param[in] hSession The session's handle
+/// @param[in] pPart Signed data
+/// @param[in] ulPartLen Length of signed data
+/// @return See CKR_* values
 ///
-CK_RV C_VerifyUpdate(
-		CK_SESSION_HANDLE hSession
-		, CK_BYTE_PTR pPart
-		, CK_ULONG ulPartLen
-);
+CK_RV C_VerifyUpdate(CK_SESSION_HANDLE hSession, CK_BYTE_PTR pPart, CK_ULONG ulPartLen);
 
 ///
-/// @brief C_VerifyFinal
-/// @param hSession
-/// @param pSignature
-/// @param ulSignatureLen
-/// @return
+/// @brief Finish a multiple-part verification operation, checking the signature
+/// @param[in] hSession The session's handle
+/// @param[in] pSignature Signature to verify
+/// @param[in] ulSignatureLen Signature length
+/// @return See CKR_* values
 ///
-CK_RV C_VerifyFinal(
-		CK_SESSION_HANDLE hSession
-		, CK_BYTE_PTR pSignature
-		, CK_ULONG ulSignatureLen
-);
+CK_RV C_VerifyFinal(CK_SESSION_HANDLE hSession, CK_BYTE_PTR pSignature
+		, CK_ULONG ulSignatureLen);
 
 ///
-/// @brief C_VerifyRecoverInit
-/// @param hSession
-/// @param pMechanism
-/// @param hKey
-/// @return
+/// @brief Initialize a signature verification operation, where the data is recovered
+/// from the signature
+/// @param[in] hSession The session's handle
+/// @param[in] pMechanism The verification mechanism
+/// @param[in] hKey Verification key
+/// @return See CKR_* values
 ///
-CK_RV C_VerifyRecoverInit(
-		CK_SESSION_HANDLE hSession
-		, CK_MECHANISM_PTR pMechanism
-		, CK_OBJECT_HANDLE hKey
-);
+CK_RV C_VerifyRecoverInit(CK_SESSION_HANDLE hSession, CK_MECHANISM_PTR pMechanism
+		, CK_OBJECT_HANDLE hKey);
 
 ///
-/// @brief C_VerifyRecover
-/// @param hSession
-/// @param pSignature
-/// @param ulSignatureLen
-/// @param pData
-/// @param pulDataLen
-/// @return
+/// @brief Verify a signature in a single-part operation, where the data is recovered
+/// from the signature
+/// @param[in] hSession The session's handle
+/// @param[in] pSignature Signature to verify
+/// @param[in] ulSignatureLen Signature length
+/// @param[out] pData Gets signed data
+/// @param[out] pulDataLen Gets signed data length
+/// @return See CKR_* values
 ///
-CK_RV C_VerifyRecover(
-		CK_SESSION_HANDLE hSession
-		, CK_BYTE_PTR pSignature
-		, CK_ULONG ulSignatureLen
-		, CK_BYTE_PTR pData
-		, CK_ULONG_PTR pulDataLen
-);
+CK_RV C_VerifyRecover(CK_SESSION_HANDLE hSession
+		, CK_BYTE_PTR pSignature, CK_ULONG ulSignatureLen
+		, CK_BYTE_PTR pData, CK_ULONG_PTR pulDataLen);
 
 #ifdef __cplusplus
 }
