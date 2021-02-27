feb65a0acfebaa2ecac057688f5213ae65a40a78
Anton Kashcheev <antonkachsheev@gmail.com>
2021-02-27 22:22:49 +0300

    Add documentation to Interface/OtherFunctions.hpp

--- src/Crypto/SoftPkcs11/Interface/OtherFunctions.hpp
+++ src/Crypto/SoftPkcs11/Interface/OtherFunctions.hpp
@@ -9,30 +9,23 @@ extern "C"
 #endif
 
 ///
-/// @brief C_SeedRandom
-/// @param hSession
-/// @param pSeed
-/// @param ulSeedLen
-/// @return
+/// @brief Mix additional seed material into the token's random number generator
+/// @param[in] hSession The session's handle
+/// @param[in] pSeed The seed material
+/// @param[in] ulSeedLen Length of seed material
+/// @return See CKR_* values
 ///
-CK_RV C_SeedRandom(
-		CK_SESSION_HANDLE hSession
-		, CK_BYTE_PTR pSeed
-		, CK_ULONG ulSeedLen
-);
+CK_RV C_SeedRandom(CK_SESSION_HANDLE hSession, CK_BYTE_PTR pSeed, CK_ULONG ulSeedLen);
 
 ///
-/// @brief C_GenerateRandom
-/// @param hSession
-/// @param RandomData
-/// @param ulRandomLen
-/// @return
+/// @brief Generate random data
+/// @param[in] hSession The session's handle
+/// @param[out] RandomData Receives the random data
+/// @param[in] ulRandomLen Count of bytes to generate
+/// @return See CKR_* values
 ///
-CK_RV C_GenerateRandom(
-		CK_SESSION_HANDLE hSession
-		, CK_BYTE_PTR RandomData
-		, CK_ULONG ulRandomLen
-);
+CK_RV C_GenerateRandom(CK_SESSION_HANDLE hSession, CK_BYTE_PTR RandomData
+		, CK_ULONG ulRandomLen);
 
 #ifdef __cplusplus
 }
