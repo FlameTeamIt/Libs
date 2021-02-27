f6cd359d38a53ba82a8d37e6c6beb00ae57ef2f7
Anton Kashcheev <antonkachsheev@gmail.com>
2021-02-27 22:22:27 +0300

    Add documentation to Interface/ParallelManagementFunctions.hpp

--- src/Crypto/SoftPkcs11/Interface/ParallelManagementFunctions.hpp
+++ src/Crypto/SoftPkcs11/Interface/ParallelManagementFunctions.hpp
@@ -9,31 +9,28 @@ extern "C"
 #endif
 
 ///
-/// @brief C_GetFunctionStatus
-/// @param hSession
-/// @return
+/// @brief Legacy function; it obtains an updated status of a function running in
+/// parallel with an application
+/// @param[in] hSession The session's handle
+/// @return See CKR_* values
 ///
 CK_RV C_GetFunctionStatus(CK_SESSION_HANDLE hSession);
 
 ///
-/// @brief C_CancelFunction
-/// @param hSession
-/// @return
+/// @brief Legacy function; it cancels a function running in parallel
+/// @param[in] hSession The session's handle
+/// @return See CKR_* values
 ///
 CK_RV C_CancelFunction(CK_SESSION_HANDLE hSession);
 
 ///
-/// @brief C_WaitForSlotEvent
-/// @param flags
-/// @param pSlot
-/// @param pReserved
-/// @return
+/// @brief Wait for a slot event (token insertion, removal, etc.) to occur
+/// @param[in] flags Blocking/nonblocking flag
+/// @param[out] pSlot Location that receives the slot ID
+/// @param[in] pReserved Reserved. Should be NULL_PTR
+/// @return See CKR_* values
 ///
-CK_RV C_WaitForSlotEvent(
-		CK_FLAGS flags
-		, CK_SLOT_ID_PTR pSlot
-		, CK_VOID_PTR pReserved
-);
+CK_RV C_WaitForSlotEvent(CK_FLAGS flags, CK_SLOT_ID_PTR pSlot, CK_VOID_PTR pReserved);
 
 #ifdef __cplusplus
 }
