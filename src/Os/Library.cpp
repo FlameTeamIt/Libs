a01b413b11d5f5057a655daf38af0a3ac6def71f
Anton Kashcheev <antonkachsheev@gmail.com>
2021-03-07 04:22:21 +0300

    src/Os/Library.cpp: add funciton for getting LibraryHandle; fix contructor

--- src/Os/Library.cpp
+++ src/Os/Library.cpp
@@ -2,9 +2,74 @@
 
 #include <FlameIDE/../../src/Os/LibraryFunctions.hpp>
 
+#include <FlameIDE/Templates/Array.hpp>
+#include <FlameIDE/Templates/String.hpp>
+
 namespace flame_ide
 {namespace os
 {
+
+namespace
+{
+
+LibraryHandle getHandle(const char *name, const char *directory)
+{
+	constexpr size_t maxPathLength = 2048;
+
+	const char *libraryPrefix = os::LIBRARY_PREFIX;
+	const char *librarySuffixShared = os::LIBRARY_SUFFIX_SHARED;
+
+	const size_t directoryLength = templates::String::rawStringLength(directory);
+	const size_t nameLength = templates::String::rawStringLength(name);
+	const size_t nameFullLength = templates::String::rawStringLength(libraryPrefix)
+			+ nameLength
+			+ templates::String::rawStringLength(librarySuffixShared);
+
+	const size_t fullPathLength = directoryLength + 1 + nameFullLength; // + slash
+	if (fullPathLength > maxPathLength)
+	{
+		templates::Array<char, maxPathLength> fullPathStatic;
+		if (directory)
+		{
+			fullPathStatic.insert(fullPathStatic.end()
+					, directory, directory + directoryLength);
+			fullPathStatic.pushBack(DIRECTORY_SLASH);
+		}
+		fullPathStatic.insert(fullPathStatic.end()
+				, libraryPrefix
+				, libraryPrefix + templates::String::rawStringLength(libraryPrefix));
+		fullPathStatic.insert(fullPathStatic.end()
+				, name, name + nameLength);
+		fullPathStatic.insert(
+				fullPathStatic.end()
+				, librarySuffixShared
+				, librarySuffixShared + templates::String::rawStringLength(
+						librarySuffixShared
+				)
+		);
+		fullPathStatic.insert(fullPathStatic.end(), char{});
+
+		return library::open(fullPathStatic.data());
+	}
+	else
+	{
+		templates::String fullPathString;
+		fullPathString.reserve(fullPathLength + 1);
+		if (directory)
+		{
+			fullPathString += directory;
+			fullPathString += DIRECTORY_SLASH;
+		}
+		fullPathString.insert(fullPathString.end(), libraryPrefix);
+		fullPathString.insert(fullPathString.end(), name);
+		fullPathString.insert(fullPathString.end(), librarySuffixShared);
+
+		return library::open(fullPathString.data());
+	}
+
+}
+
+} // namespace anonymous
 
 Library::Library() noexcept : Library{ LIBRARY_HANDLE_INVALID }
 {}
@@ -14,8 +79,8 @@ Library::Library(Library &&library) noexcept : handle{ library.handle }
 	library.handle = LIBRARY_HANDLE_INVALID;
 }
 
-Library::Library(const char *libraryPath)
-		: handle{ library::open(libraryPath) }
+Library::Library(const char *name, const char *directory)
+		: handle{ getHandle(name, directory) }
 {}
 
 Library::Library(LibraryHandle libraryHandle) noexcept
