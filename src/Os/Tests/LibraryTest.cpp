07c288f544dd474f7dfb06a12d88a4c06aa7ce81
Anton Kashcheev <antonkachsheev@gmail.com>
2021-03-07 04:22:48 +0300

    Os/Tests/LibraryTest.cpp: add implementation of test

--- src/Os/Tests/LibraryTest.cpp
+++ src/Os/Tests/LibraryTest.cpp
@@ -1,5 +1,9 @@
 #include <FlameIDE/../../src/Os/Tests/LibraryTest.hpp>
 
+#include <FlameIDE/Os/Library.hpp>
+
+#include <FlameIDE/Crypto/Pkcs11/Types.hpp>
+
 namespace flame_ide
 {namespace os
 {namespace tests
@@ -12,6 +16,20 @@ LibraryTest::~LibraryTest() = default;
 
 int LibraryTest::vStart()
 {
+	Library library("SoftPkcs11");
+	if (!library)
+	{
+		return FAILED;
+	}
+
+	auto getfuncitonList = library.find<pkcs11::callbacks::GetFunctionList>(
+			"C_GetFunctionList"
+	);
+	if (!getfuncitonList)
+	{
+		return FAILED;
+	}
+
 	return SUCCESS;
 }
 
