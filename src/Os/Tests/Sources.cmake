b48c776a074079c4a5078193c56600df6d47a03c
Anton Kashcheev <antonkachsheev@gmail.com>
2021-04-28 00:47:18 +0300

    Add main.cpp to Os/Tests module

--- src/Os/Tests/Sources.cmake
+++ src/Os/Tests/Sources.cmake
@@ -3,4 +3,5 @@ set (SOURCE_LIST
 	${CMAKE_CURRENT_SOURCE_DIR}/LibraryTest.hpp
 	${CMAKE_CURRENT_SOURCE_DIR}/TestAggregator.cpp
 	${CMAKE_CURRENT_SOURCE_DIR}/TestAggregator.hpp
+	${CMAKE_CURRENT_SOURCE_DIR}/main.cpp
 )
