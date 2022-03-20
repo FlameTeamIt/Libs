ef49cd5412681504346b5b57b71fc8db465322ac
U-KA-855G7-WIN10\anton <antonkachsheev@gmail.com>
2022-03-20 03:13:40 +0300

    Add return values to doxygen comments for general purpose functions

--- src/Crypto/SoftPkcs11/Interface/GeneralPurposeFunctions.hpp
+++ src/Crypto/SoftPkcs11/Interface/GeneralPurposeFunctions.hpp
@@ -12,28 +12,49 @@ extern "C"
 /// @brief Initializes the Cryptoki library
 /// @param[in] pInitArgs if this is not NULL_PTR, it gets cast to
 /// CK_C_INITIALIZE_ARGS_PTR and dereferenced
-/// @return See CKR_* values
+/// @return CKR_ARGUMENTS_BAD,
+///         CKR_CANT_LOCK,
+///         CKR_CRYPTOKI_ALREADY_INITIALIZED,
+///         CKR_FUNCTION_FAILED,
+///         CKR_GENERAL_ERROR,
+///         CKR_HOST_MEMORY,
+///         CKR_NEED_TO_CREATE_THREADS,
+///         CKR_OK
 ///
 CK_RV C_Initialize(CK_VOID_PTR pInitArgs);
 
 ///
 /// @brief Indicates that an application is done with the Cryptoki library
 /// @param pReserved reserved; should be NULL_PTR
-/// @return See CKR_* values
+/// @return CKR_ARGUMENTS_BAD,
+///         CKR_CRYPTOKI_NOT_INITIALIZED,
+///         CKR_FUNCTION_FAILED,
+///         CKR_GENERAL_ERROR,
+///         CKR_HOST_MEMORY,
+///         CKR_OK
 ///
 CK_RV C_Finalize(CK_VOID_PTR pReserved);
 
 ///
 /// @brief Returns general information about Cryptoki
 /// @param[out] pInfo Location that receives information
-/// @return See CKR_* values
+/// @return CKR_ARGUMENTS_BAD,
+///         CKR_CRYPTOKI_NOT_INITIALIZED,
+///         CKR_FUNCTION_FAILED,
+///         CKR_GENERAL_ERROR,
+///         CKR_HOST_MEMORY,
+///         CKR_OK
 ///
 CK_RV C_GetInfo(CK_INFO_PTR pInfo);
 
 ///
 /// @brief Returns the function list
 /// @param ppFunctionList receives pointer to function list
-/// @return See CKR_* values
+/// @return CKR_ARGUMENTS_BAD,
+///         CKR_FUNCTION_FAILED,
+///         CKR_GENERAL_ERROR,
+///         CKR_HOST_MEMORY,
+///         CKR_OK
 ///
 CK_RV C_GetFunctionList(CK_FUNCTION_LIST_PTR_PTR ppFunctionList);
 
@@ -43,7 +64,12 @@ CK_RV C_GetFunctionList(CK_FUNCTION_LIST_PTR_PTR ppFunctionList);
 /// @brief Returns all the interfaces supported by the module
 /// @param pInterfacesList Returned interfaces
 /// @param pulCount Number of interfaces returned
-/// @return See CKR_* values
+/// @return CKR_BUFFER_TOO_SMALL,
+///         CKR_ARGUMENTS_BAD,
+///         CKR_FUNCTION_FAILED,
+///         CKR_GENERAL_ERROR,
+///         CKR_HOST_MEMORY,
+///         CKR_OK
 ///
 CK_RV C_GetInterfaceList(CK_INTERFACE_PTR pInterfacesList, CK_ULONG_PTR pulCount);
 
@@ -53,7 +79,12 @@ CK_RV C_GetInterfaceList(CK_INTERFACE_PTR pInterfacesList, CK_ULONG_PTR pulCount
 /// @param pVersion Version of the interface
 /// @param ppInterface Returned interface
 /// @param flags Flags controlling the semantics of the interface
-/// @return See CKR_* values
+/// @return CKR_BUFFER_TOO_SMALL,
+///         CKR_ARGUMENTS_BAD,
+///         CKR_FUNCTION_FAILED,
+///         CKR_GENERAL_ERROR,
+///         CKR_HOST_MEMORY,
+///         CKR_OK
 ///
 CK_RV C_GetInterface(CK_UTF8CHAR_PTR pInterfaceName, CK_VERSION_PTR pVersion,
 		CK_INTERFACE_PTR_PTR ppInterface, CK_FLAGS flags);
