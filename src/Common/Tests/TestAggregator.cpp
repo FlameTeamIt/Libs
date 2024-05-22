772ae8edeb6d9725930f2943fc8ced4748920d6a
Anton Kashcheev <antonkachsheev@gmail.com>
2024-05-23 03:37:33 +0500

    Common: add VoidType and VoidMaker (#7)

--- src/Common/Tests/TestAggregator.cpp
+++ src/Common/Tests/TestAggregator.cpp
@@ -1,5 +1,6 @@
 #include <FlameIDE/../../src/Common/Tests/UtilsTest.hpp>
 #include <FlameIDE/../../src/Common/Tests/ExpectedTest.hpp>
+#include <FlameIDE/../../src/Common/Tests/VoidTypeTest.hpp>
 
 #include <FlameIDE/../../src/Common/Tests/TestAggregator.hpp>
 
@@ -12,6 +13,7 @@ TestAggregator::TestAggregator() : ::TestAggregator("Common")
 {
 	pushBackTest(std::make_shared<UtilsTest>());
 	pushBackTest(std::make_shared<ExpectedTest>());
+	pushBackTest(std::make_shared<VoidTypeTest>());
 }
 
 }}} // namespace flame_ide::common::tests
